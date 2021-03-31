4fa281a8e9d0ca9a3a74b301cfc6f7cbe430f6be
Albert-Jan Yzelman <yzelman@linux.fritz.box>
2021-03-31 14:56:40 +0200

    Much improved Pregel-style API

--- include/graphblas/interfaces/pregel.hpp
+++ include/graphblas/interfaces/pregel.hpp
@@ -13,43 +13,31 @@
 #include <graphblas/utils/parser.hpp>
 
 #include <stdexcept>   // std::runtime_error
-#include <functional>  // std::function
 
 namespace grb {
 
 	namespace interfaces {
 
+		struct PregelData {
+			bool &active;
+			const size_t &num_vertices;
+			const size_t &outdegree;
+			const size_t &indegree;
+			const size_t &round;
+		};
+
 		template<
-			typename MessageType,
-			template< typename, typename, typename > class CombinerOp,
-			class CombinerIdentity,
-			typename CombinedType = MessageType
+			typename MatrixEntryType
 		>
 		class Pregel {
 
 			private:
 
-				typedef CombinerOp< MessageType, CombinedType, CombinedType > Combiner;
-
-				static_assert( grb::is_operator< Combiner >::value,
-					"The combiner must be a GraphBLAS operator"
-				);
-
-				static_assert( grb::is_associative< Combiner >::value,
-					"The combiner must be associative."
-				);
-
 				/** The number of vertices. */
 				const size_t n;
 
 				/** The graph */
-				grb::Matrix< void > graph;
-
-				/** Incoming messages after broadcast */
-				grb::Vector< MessageType > in;
-
-				/** Outgoing messages for broadcast */
-				grb::Vector< MessageType > out;
+				grb::Matrix< MatrixEntryType > graph;
 
 				/** Which vertices are still active */
 				grb::Vector< bool > activeVertices;
@@ -57,100 +45,125 @@ namespace grb {
 				/** Pre-computed outdegrees. */
 				grb::Vector< size_t > outdegrees;
 
+				/** Pre-cominputed indegrees. */
+				grb::Vector< size_t > indegrees;
+
 				/** 
 				 * Initialises the following fields:
 				 *   -# outdegrees
 				 */
 				void initialize() {
-					grb::Semiring< size_t > intRing;
+					grb::Semiring<
+						size_t, size_t, size_t, size_t,
+						grb::operators::add,
+						grb::operators::right_assign,
+						grb::identities::zero,
+						grb::identities::zero
+					> patternRing;
 					grb::Vector< size_t > ones( n );
 					if( grb::set( ones, 1 ) != SUCCESS ) {
 						throw std::runtime_error( "Could not set vector ones" );
 					}
-					if( grb::set( outdegrees, 1 ) != SUCCESS ) {
+					if( grb::set( outdegrees, 0 ) != SUCCESS ) {
 						throw std::runtime_error( "Could not initialise outdegrees" );
 					}
 					if( grb::vxm< grb::descriptors::in_place >(
-						outdegrees, ones, graph, intRing
+						outdegrees, ones, graph, patternRing
 					) != SUCCESS ) {
 						throw std::runtime_error( "Could not compute outdegrees" );
+					}
+					if( grb::set( indegrees, 0 ) != SUCCESS ) {
+						throw std::runtime_error( "Could not initialise indegrees" );
+					}
+					if( grb::mxv< grb::descriptors::in_place >(
+						indegrees, graph, ones, patternRing
+					) != SUCCESS ) {
+						throw std::runtime_error( "Could not compute indegrees" );
 					}
 				}
 
 
 			public:
 
-				struct PregelData {
-					const CombinedType &incoming_message;
-					MessageType &outgoing_message;
-					bool &active;
-					const size_t &num_vertices;
-					const size_t &outdegree;
-					const size_t &round;
-				};
-
-				template< typename InputType >
-				Pregel( const grb::Matrix< InputType > &_g ) :
-					n( grb::nrows( _g ) ),
-					graph( n, grb::ncols( _g ) ),
-					in( n ), out( n ),
-					activeVertices( n ),
-					outdegrees( n )
+				template< typename fwd_it >
+				Pregel(
+					const size_t _m, const size_t _n,
+					fwd_it _start, const fwd_it _end
+				) :
+					n( _m ),
+					graph( _m, _n ),
+					activeVertices( _n ),
+					outdegrees( _n ),
+					indegrees( _n )
 				{
 					if( grb::ncols( graph ) != grb::nrows( graph ) ) {
 						throw std::runtime_error( "Input graph is bipartite" );
 					}
 					if( grb::buildMatrixUnique(
-						graph,
-						_g.begin(),
-						_g.end()
+						graph, _start, _end, SEQUENTIAL
 					) != SUCCESS ) {
-						throw std::runtime_error( "Could not copy input graph" );
+						throw std::runtime_error( "Could not build graph" );
 					}
 					initialize();
 				}
 
-				Pregel( const std::string &filename ) : Pregel(
-					grb::utils::MatrixFileReader< void >( filename )
-				) {};
-
-				Pregel( grb::utils::MatrixFileReader< void > file ) :
-					n( file.n() ),
-					graph( n, file.m() ),
-					in( n ), out( n ),
-					activeVertices( n ),
-					outdegrees( n )
-				{
-					if( grb::ncols( graph ) != grb::nrows( graph ) ) {
-						throw std::runtime_error( "Input graph is bipartite" );
-					}
-					if( grb::buildMatrixUnique(
-						graph,
-						file.begin(),
-						file.end(),
-						SEQUENTIAL
-					) != SUCCESS ) {
-						throw std::runtime_error( "Error during ingestion of input graph" );
-					}
-					initialize();
-				}
-
+				/**
+				 * @param[in] in  Where incoming messages are stored. This can hold default
+				 *                messages on function entry valid for the first round. If
+				 *                \a in does not contain #num_vertices entries it will
+				 *                instead be reset to the given identity.
+				 */
 				template<
 					typename IOType,
-					typename GlobalProgramData
+					typename IncomingMessageType,
+					typename OutgoingMessageType,
+					typename GlobalProgramData,
+					template< typename, typename, typename > class Combiner,
+					class CombinerIdentity,
+					class Program
 				>
 				grb::RC execute(
 					grb::Vector< IOType > &x,
-					std::function< void(
-						IOType&,
-						const GlobalProgramData&,
-						PregelData&
-					)> program,
+					grb::Vector< IncomingMessageType > &in,
+					grb::Vector< OutgoingMessageType > &out,
+					Program program,
 					const GlobalProgramData &data,
+					const Combiner<
+						IncomingMessageType,
+						OutgoingMessageType,
+						OutgoingMessageType
+					> &combiner = Combiner<
+						IncomingMessageType,
+						OutgoingMessageType,
+						OutgoingMessageType
+					>(),
+					const CombinerIdentity &identity = CombinerIdentity(),
 					const size_t max_steps = 1000
 				) {
+					static_assert( grb::is_operator< Combiner<
+							IncomingMessageType,
+							OutgoingMessageType,
+							OutgoingMessageType
+						> >::value,
+						"The combiner must be a GraphBLAS operator"
+					);
+
+					static_assert( grb::is_associative< Combiner<
+							IncomingMessageType,
+							OutgoingMessageType,
+							OutgoingMessageType
+						> >::value,
+						"The combiner must be associative."
+					);
+
 					// sanity checks
 					if( grb::size(x) != n ) {
+						return MISMATCH;
+					}
+					if( grb::size(in) != n ) {
+						return MISMATCH;
+					}
+					if( grb::size(out) != n ) {
 						return MISMATCH;
 					}
 
@@ -161,11 +174,16 @@ namespace grb {
 					> orMonoid;
 
 					grb::Semiring<
-						MessageType, MessageType, MessageType, MessageType,
-						CombinerOp,
+						bool,
+						IncomingMessageType,
+						IncomingMessageType,
+						OutgoingMessageType,
+						Combiner,
+						//grb::operators::right_assign_if_left,
 						grb::operators::right_assign,
 						CombinerIdentity,
-						CombinerIdentity
+						//grb::identities::logical_true
+						grb::identities::zero
 					> ring;
 
 					// set initial round ID
@@ -175,13 +193,18 @@ namespace grb {
 					grb::RC ret = grb::set( activeVertices, true );
 
 					// set default incoming message
-					if( ret == SUCCESS ) {
-						ret = grb::set( in, ring.template getZero< CombinedType >() );
+					if( ret == SUCCESS && grb::nnz(in) < n ) {
+#ifdef _DEBUG
+						if( grb::nnz(in) > 0 ) {
+							std::cerr << "Warning: overwriting initial incoming messages because it was not a dense vector\n";
+						}
+#endif
+						ret = grb::set( in, ring.template getZero< IncomingMessageType >() );
 					}
 
 					// set default outgoing message
 					if( ret == SUCCESS ) {
-						ret = grb::set( out, ring.template getZero< MessageType >() );
+						ret = grb::set( out, ring.template getZero< OutgoingMessageType >() );
 					}
 
 					// check if there are active vertices
@@ -204,27 +227,35 @@ namespace grb {
 							[
 								this,
 								&x,
+								&in,
+								&out,
 								&program,
 								&step,
 								&data
 							]( const size_t i ) {
 								// create Pregel struct
 								PregelData pregel = {
-									in[ i ],
-									out[ i ],
 									activeVertices[ i ],
 									n,
 									outdegrees[ i ],
+									indegrees[ i ],
 									step
 								};
 								// only execute program on active vertices
 								if( activeVertices[ i ] ) {
+#ifdef _DEBUG
 									std::cout << "Vertex " << i << " remains active in step " << step << "\n";
+#endif
 									program(
 										x[ i ],
+										in[ i ],
+										out[ i ],
 										data,
 										pregel
 									);
+#ifdef _DEBUG
+									std::cout << "Vertex " << i << " sends out message " << out[ i ] << "\n";
+#endif
 								}
 							}, x, activeVertices, in, out, outdegrees
 						);
@@ -233,7 +264,7 @@ namespace grb {
 							ret = grb::set(
 								in,
 								//activeVertices,
-								ring.template getZero< CombinedType >()
+								ring.template getZero< IncomingMessageType >()
 							);
 						}
 						// execute communication
@@ -242,7 +273,7 @@ namespace grb {
 						}
 						// reset outgoing buffer
 						if( ret == SUCCESS ) {
-							ret = grb::set( out, ring.template getZero< MessageType >() );
+							ret = grb::set( out, ring.template getZero< OutgoingMessageType >() );
 						}
 						// check if there is a next round
 						if( ret == SUCCESS ) {
@@ -256,13 +287,16 @@ namespace grb {
 						(void) ++step;
 					}
 
-					std::cout << "Pregel exits after " << step << " steps with error code " << ret << " ( " << grb::toString(ret) << " )\n";
+					std::cout << "Info: Pregel exits after " << step << " steps with error code " << ret << " ( " << grb::toString(ret) << " )\n";
 
 					// done
 					return ret;
 				}
 
 				size_t num_vertices() const noexcept { return n; }
+
+				const grb::Matrix< MatrixEntryType >&
+				get_matrix() const noexcept { return graph; }
 
 		};
 
