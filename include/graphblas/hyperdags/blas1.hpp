335786eb8a4cadf997e63df034bef9a7b865a2a7
Albert-Jan Yzelman <yzelman@linux.fritz.box>
2022-01-31 17:28:52 +0100

    Issue #274 add nnz and set on vectors

---
+++ include/graphblas/hyperdags/blas1.hpp
@@ -0,0 +1,52 @@
+
+/*
+ *   Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * @author A. N. Yzelman
+ * @date 31st of January, 2022
+ */
+
+#ifndef _H_GRB_HYPERDAGS_BLAS1
+#define _H_GRB_HYPERDAGS_BLAS1
+
+#include "vector.hpp"
+
+
+namespace grb {
+
+	template< typename DataType, typename Coords >
+	size_t nnz( const Vector< DataType, hyperdags, Coords > &x ) noexcept {
+		return nnz( internal::getVector( x ) );
+	}
+
+	template<
+		Descriptor descr = descriptors::no_operation,
+		typename DataType, typename Coords,
+		typename T
+	>
+	RC set( Vector< DataType, hyperdags, Coords > &x, const T val,
+		const typename std::enable_if< !grb::is_object< DataType >::value &&
+			!grb::is_object< T >::value,
+		void >::type * const = nullptr
+	) {
+		return set( internal::getVector( x ), val );
+	}
+
+} // end namespace grb
+
+#endif
+
