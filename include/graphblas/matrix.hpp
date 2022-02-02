af9516e249fdf4ec335232513305a151380b1d20
Daniele Giuseppe Spampinato <daniele.giuseppe.spampinato@huawei.com>
2022-02-02 10:23:26 +0100

    Drafting structured matrix interface using reference_dense backend

--- include/graphblas/matrix.hpp
+++ include/graphblas/matrix.hpp
@@ -28,14 +28,13 @@
 
 // now include all specialisations contained in the backend directories:
 #ifdef _GRB_WITH_REFERENCE
- #include <graphblas/reference/matrix.hpp>
- #include <graphblas/denseref/matrix.hpp>
+#include <graphblas/reference/matrix.hpp>
 #endif
 #ifdef _GRB_WITH_LPF
- #include <graphblas/bsp1d/matrix.hpp>
+#include <graphblas/bsp1d/matrix.hpp>
 #endif
 #ifdef _GRB_WITH_BANSHEE
- #include <graphblas/banshee/matrix.hpp>
+#include <graphblas/banshee/matrix.hpp>
 #endif
 
 // specify default only if requested during compilation
@@ -48,7 +47,14 @@ namespace grb {
 		typename NonzeroIndexType = config::NonzeroIndexType
 	>
 	class Matrix;
-}
+
+	/*
+	 * The default value of \a StorageSchemeType could also be made conditional (Dense or Sparse) depending on \a config::default_backend
+	 */
+	template< typename T, typename Structure, typename StorageSchemeType = storage::Dense, typename View = view::Identity< void >, enum Backend backend = config::default_backend >
+	class StructuredMatrix;
+
+} // namespace grb
 #endif
 
 #endif // end ``_H_GRB_MATRIX''
