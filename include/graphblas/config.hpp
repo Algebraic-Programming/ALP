152180de63efd3afb3d3155206c6ddfedcb906d3
Daniele Giuseppe Spampinato <daniele.giuseppe.spampinato@huawei.com>
2022-02-21 20:28:42 +0100

    Basic VectorView definition based on grb::vector container

--- include/graphblas/config.hpp
+++ include/graphblas/config.hpp
@@ -29,7 +29,7 @@
 #ifdef _GRB_WITH_REFERENCE
  #include "graphblas/reference/config.hpp"
 #endif
-#ifdef _GRB_WITH_DENSE
+#ifdef _GRB_WITH_DENSEREF
  #include "graphblas/denseref/config.hpp"
 #endif
 #ifdef _GRB_WITH_HYPERDAGS
