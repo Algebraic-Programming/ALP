c4f456e44b1a756f4fa6a8c834b8cff377372e08
vdimic <10420927+vdimic@user.noreply.gitee.com>
2022-03-14 10:24:12 +0000

    !19 Resolve internal issue #360: Make VectorView "compatible" with StructuredMatrix in terms of template parameters This commit mainly adds Structure to the VectorView and reorders VectorView's template parameters to follow the same order as in StructuredMatrix. Doxygen is updated to reflect these changes.
    In addition, there are the following changes:
    * Remove VectorView's Tmp parameter from blas1 doxygen
    * Use consistent ordering of template name parameters across blas 1/2/3. The order is Output, Mask, Inputs.
    * Fix minor unrelated issues
    * Generalize over storage scheme type for VectorView in level 1/2/3 routines

--- include/graphblas/base/vector.hpp
+++ include/graphblas/base/vector.hpp
@@ -875,7 +875,7 @@ namespace grb {
 			}
 };
 
-	template< typename T, typename View, typename StorageSchemeType, enum Backend backend, typename C >
+	template< typename T, typename Structure, typename StorageSchemeType, typename View, enum Backend backend, typename C >
 	class VectorView { };
 
 }
