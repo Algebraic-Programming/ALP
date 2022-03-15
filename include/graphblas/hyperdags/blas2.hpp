c763ce79a6d8b8ed842590b3ab1c091680975739
Katerina Karanasiou <akaranasiou@githuaweirc.com>
2022-03-15 23:51:23 +0100

    functions for outer test

--- include/graphblas/hyperdags/blas2.hpp
+++ include/graphblas/hyperdags/blas2.hpp
@@ -49,9 +49,87 @@ namespace grb {
 		return nnz(internal::getMatrix(A));
 	}
 	
-	template< typename InputType >
+	template<
+		 typename InputType 
+	>
 	RC resize( Matrix< InputType, hyperdags > & A, const size_t new_nz ) noexcept {
 		return resize(internal::getMatrix(A), new_nz);
+	}
+	
+	template< 
+		Descriptor descr = descriptors::no_operation, class Ring, typename IOType,
+		typename InputType1, typename InputType2, typename InputType3, typename Coords 
+	>
+	RC vxm( Vector< IOType, hyperdags, Coords > & u,
+		const Vector< InputType3, hyperdags, Coords > & mask,
+		const Vector< InputType1, hyperdags, Coords > & v,
+		const Matrix< InputType2, hyperdags > & A,
+		const Ring & ring = Ring(),
+		const typename std::enable_if< grb::is_semiring< Ring >::value, void >::type * const = NULL ) {
+		
+		std::array< const void *, 3 > sources{ &mask, &v, &A};
+		std::array< const void *, 1 > destinations{ &u };
+		internal::hyperdags::generator.addOperation(
+				internal::hyperdags::VXM_VECTOR_VECTOR_VECTOR_MATRIX,
+				sources.begin(), sources.end(),
+				destinations.begin(), destinations.end()
+		);
+		return vxm< descr>( internal::getVector(u), internal::getVector(mask), internal::getVector(v), internal::getMatrix(A), ring );
+	}
+	
+	
+	template< 
+		Descriptor descr = descriptors::no_operation,
+		class AdditiveMonoid,
+		class MultiplicativeOperator,
+		typename IOType,
+		typename InputType1,
+		typename InputType2,
+		typename InputType3,
+		typename Coords 
+	>
+	RC vxm( Vector< IOType, hyperdags, Coords > & u,
+		const Vector< InputType3, hyperdags, Coords > & mask,
+		const Vector< InputType1, hyperdags, Coords > & v,
+		const Matrix< InputType2, hyperdags > & A,
+		const AdditiveMonoid & add = AdditiveMonoid(),
+		const MultiplicativeOperator & mul = MultiplicativeOperator(),
+		const typename std::enable_if< grb::is_monoid< AdditiveMonoid >::value && grb::is_operator< MultiplicativeOperator >::value && ! grb::is_object< IOType >::value &&
+				! grb::is_object< InputType1 >::value && ! grb::is_object< InputType2 >::value && ! grb::is_object< InputType3 >::value && ! std::is_same< InputType2, void >::value,
+			void >::type * const = NULL ) {
+			
+		std::array< const void *, 3 > sources{ &mask, &v, &A};
+		std::array< const void *, 1 > destinations{ &u };
+		internal::hyperdags::generator.addOperation(
+				internal::hyperdags::VXM_VECTOR_VECTOR_VECTOR_MATRIX_ADD_MUL,
+				sources.begin(), sources.end(),
+				destinations.begin(), destinations.end()
+		);
+		
+		return vxm< descr>( internal::getVector(u), internal::getVector(mask), internal::getVector(v), internal::getMatrix(A), add, mul );
+	}
+
+	template< Descriptor descr = descriptors::no_operation,
+		class Ring,
+		typename IOType = typename Ring::D4,
+		typename InputType1 = typename Ring::D1,
+		typename InputType2 = typename Ring::D2,
+		typename Coords 	
+	>
+	RC vxm( Vector< IOType, hyperdags, Coords > & u,
+		const Vector< InputType1, hyperdags, Coords > & v,
+		const Matrix< InputType2, hyperdags > & A,
+		const Ring & ring = Ring(),
+		const typename std::enable_if< grb::is_semiring< Ring >::value, void >::type * const = NULL ) {
+		
+		std::array< const void *, 2 > sources{ &v, &A};
+		std::array< const void *, 1 > destinations{ &u };
+		internal::hyperdags::generator.addOperation(
+				internal::hyperdags::VXM_VECTOR_VECTOR_MATRIX_RING,
+				sources.begin(), sources.end(),
+				destinations.begin(), destinations.end()
+		);
+		return vxm< descr >( internal::getVector(u), internal::getVector(v), internal::getMatrix(A), ring );
 	}
 
 } // end namespace grb
