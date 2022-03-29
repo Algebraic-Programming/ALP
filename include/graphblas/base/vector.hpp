0b0d7fb577affb140b0fdcaefa9b11b7476bc2fe
Anonymous <anon@example.com>
2022-03-29 08:35:11 +0000

    !32 Resolve internal issue #378: Convert grb storage from namespace to enum
    This enum now has two values - Dense and Sparse.
    The actual implementation of the storage scheme
    will be handled by the Storage Mapping Functions.

--- include/graphblas/base/vector.hpp
+++ include/graphblas/base/vector.hpp
@@ -32,6 +32,7 @@
 #include <graphblas/descriptors.hpp>
 #include <graphblas/ops.hpp>
 #include <graphblas/rc.hpp>
+#include <graphblas/density.hpp>
 
 namespace grb {
 
@@ -900,7 +901,7 @@ namespace grb {
 			}
 };
 
-	template< typename T, typename Structure, typename StorageSchemeType, typename View, enum Backend backend >
+	template< typename T, typename Structure, enum Density density, typename View, enum Backend backend >
 	class VectorView { };
 
 }
