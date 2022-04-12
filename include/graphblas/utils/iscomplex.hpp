1273797b93971c0ee1b6f5b01a763e98ef070788
Albert-Jan N. Yzelman <albertjan.yzelman@huawei.com>
2022-04-12 13:35:13 +0200

    MatrixMarket parser can now read complex numbers. It now also recognises the Hermitian symmetry type. TODO: implement conjugation during Hermitian read-out.

---
+++ include/graphblas/utils/iscomplex.hpp
@@ -0,0 +1,71 @@
+
+/*
+ *   Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * @author A. N. Yzelman
+ * @date 12th of April, 2022
+ */
+
+#ifndef _H_GRB_UTILS_ISCOMPLEX
+#define _H_GRB_UTILS_ISCOMPLEX
+
+#include <complex>
+
+
+namespace grb {
+
+	namespace utils {
+
+		/**
+		 * A template class used for inspecting whether a given class is of type
+		 * std::complex.
+		 *
+		 * @tparam C The type to check.
+		 *
+		 * \internal This is the base implementation which assumes \a C is not of
+		 * type std::complex.
+		 */
+		template< typename C >
+		class is_complex {
+
+			public:
+
+				/**
+				 * If \a value is <tt>false</tt>, the type will be \a C.
+				 * If \a value is <tt>true</tt>, the type will be C::value_type.
+				 */
+				typedef C type;
+
+				/** Whether the type \a C is std::complex */
+				static constexpr const bool value = false;
+
+		};
+
+		/** \internal The specialisation for std::complex types. */
+		template< typename T >
+		class is_complex< std::complex< T > > {
+			public:
+				typedef T type;
+				static constexpr const bool value = true;
+		};
+
+	} // end namespace utils
+
+} // end namespace grb
+
+#endif // end _H_GRB_UTILS_ISCOMPLEX
+
