80ec52ce4ea5c356836cc642d246a5d000ca3e21
Vladimir Dimic <vladimir.dimic@huawei.com>
2022-05-31 16:12:14 +0200

    Add IMF to vector's template parameter list

--- include/alp/base/vector.hpp
+++ include/alp/base/vector.hpp
@@ -875,7 +875,7 @@ namespace alp {
 		}; // class Vector
 	} // namespace internal
 
-	template< typename T, typename Structure, enum Density density, typename View, enum Backend backend >
+	template< typename T, typename Structure, enum Density density, typename View, typename Imf, enum Backend backend >
 	class Vector { };
 
 }
