5d11e7f75c8a22eb1501a47a79f5614009025209
Vladimir Dimic <vladimir.dimic@huawei.com>
2022-06-23 18:00:13 +0200

    Update dense_outer test

--- tests/unit/dense_outer.cpp
+++ tests/unit/dense_outer.cpp
@@ -20,33 +20,59 @@
 
 #include "alp.hpp"
 
+template< typename MatrixType >
+void print_matrix( std::string name, const MatrixType &A) {
 
-using namespace alp;
+	if( ! alp::internal::getInitialized( A ) ) {
+		std::cout << "Matrix " << name << " uninitialized.\n";
+		return;
+	}
+
+	std::cout << name << ":" << std::endl;
+	for( size_t row = 0; row < alp::nrows( A ); ++row ) {
+		std::cout << "[\t";
+		for( size_t col = 0; col < alp::ncols( A ); ++col ) {
+			auto pos  = alp::internal::getStorageIndex( A, row, col );
+			// std::cout << "(" << pos << "): ";
+			std::cout << alp::internal::access( A, pos ) << "\t";
+		}
+		std::cout << "]" << std::endl;
+	}
+}
+
 
 // alp program
-void alpProgram( const size_t &n, RC &rc ) {
-	/** \internal TODO: Implement initialization and result checking.
-	 * Currently only serves as the interface showcase.
-	 * */
+void alpProgram( const size_t &n, alp::RC &rc ) {
 
-	// semiring
-	Semiring<
-		alp::operators::add< double >, alp::operators::mul< double >,
-		alp::identities::zero, alp::identities::one
-	> ring;
+	typedef double T;
+
+	alp::Semiring< alp::operators::add< T >, alp::operators::mul< T >, alp::identities::zero, alp::identities::one > ring;
+
+	T one  = ring.getOne< T >();
+	T zero = ring.getZero< T >();
+
 	// allocate
-	Vector< double > u( n );
-	Vector< double > v( n );
-	Matrix< double, structures::General > M( n, n );
+	std::vector< T > u_data( n, one );
+	std::vector< T > v_data( n, one );
+	std::vector< T > M_data( n, zero );
 
-	rc = outer( M, u, v, ring.getMultiplicativeOperator());
+	alp::Vector< T > u( n );
+	alp::Vector< T > v( n );
+	alp::Matrix< T, alp::structures::General > M( n, n );
+
+	alp::buildVector( u, u_data.begin(), u_data.end() );
+	alp::buildVector( v, v_data.begin(), v_data.end() );
+
+	rc = alp::outer( M, u, v, ring.getMultiplicativeOperator());
 
 	// Example with matrix view on a lambda function.
-	auto uvT = outer( u, v, ring.getMultiplicativeOperator() );
+	auto uvT = alp::outer( u, v, ring.getMultiplicativeOperator() );
+	print_matrix( "uvT", uvT );
 
 	// Example when outer product takes the same vector as both inputs.
 	// This operation results in a symmetric positive definite matrix.
-	auto vvT = outer( v, ring.getMultiplicativeOperator() );
+	auto vvT = alp::outer( v, ring.getMultiplicativeOperator() );
+	print_matrix( "vvT", uvT );
 
 }
 
@@ -85,13 +111,13 @@ int main( int argc, char ** argv ) {
 	std::cout << "Functional test executable: " << argv[ 0 ] << "\n";
 
 	std::cout << "This is functional test " << argv[ 0 ] << "\n";
-	alp::Launcher< AUTOMATIC > launcher;
+	alp::Launcher< alp::AUTOMATIC > launcher;
 	alp::RC out;
-	if( launcher.exec( &alpProgram, in, out, true ) != SUCCESS ) {
+	if( launcher.exec( &alpProgram, in, out, true ) != alp::SUCCESS ) {
 		std::cerr << "Launching test FAILED\n";
 		return 255;
 	}
-	if( out != SUCCESS ) {
+	if( out != alp::SUCCESS ) {
 		std::cerr << "Test FAILED (" << alp::toString( out ) << ")" << std::endl;
 	} else {
 		std::cout << "Test OK" << std::endl;
