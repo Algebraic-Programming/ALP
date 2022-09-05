1f85945c5416c10aa792acf4faea40b035cbee2b
Denis Jelovina <denis.jelovina@huawei.com>
2022-09-05 14:13:55 +0200

    cleanup 01

--- include/alp/algorithms/cholesky.hpp
+++ include/alp/algorithms/cholesky.hpp
@@ -19,6 +19,8 @@
 
 #include <alp.hpp>
 
+#define TEMP_DISABLE
+
 namespace alp {
 
 	namespace algorithms {
@@ -43,8 +45,8 @@ namespace alp {
 			typename Minus = operators::subtract< D >,
 			typename Divide = operators::divide< D > >
 		RC cholesky_lowtr(
-			Matrix< D, structures::LowerTriangular, Dense > &L,
-			const Matrix< D, structures::SymmetricPositiveDefinite, Dense > &H,
+			Matrix< D, structures::UpperTriangular, Dense > &L,
+			const Matrix< D, structures::Symmetric, Dense > &H,
 			const Ring & ring = Ring(),
 			const Minus & minus = Minus(),
 			const Divide & divide = Divide() ) {
@@ -55,40 +57,55 @@ namespace alp {
 			const size_t n = nrows( H );
 
 			// Out of place specification of the operation
-			Matrix< D, structures::SymmetricPositiveDefinite, Dense > LL( n, n );
+			Matrix< D, structures::Symmetric, Dense > LL( n, n );
 			rc = set( LL, H );
 
 			for( size_t k = 0; k < n ; ++k ) {
-
-				const size_t m = n - k - 1;
 
 				auto v = get_view( LL , utils::range( k, n) , k );
 
 				// L[ k, k ] = alpha = sqrt( LL[ k, k ] )
 				Scalar< D > alpha;
 				rc = eWiseLambda(
-							[ &v, &alpha, &ring ]( const size_t i ) {
-								if ( i == 0 ) {
-									v[ i ] = alpha = std::sqrt( v[ i ] );
-								}
-							},
-							v );
+					[ &v, &alpha, &ring ]( const size_t i ) {
+						if ( i == 0 ) {
+#ifdef TEMP_DISABLE
+							*alpha = std::sqrt( v[ i ] );
+							internal::setInitialized( alpha, true );
+#else
+							(void)set( alpha, std::sqrt( v[ i ] ) );
+#endif
+							v[ i ] = *alpha;
+						}
+					}, v
+				);
 
 				// LL[ k + 1: , k ] = LL[ k + 1: , k ] / alpha
 				rc = eWiseLambda(
-							[ &v, &alpha, &divide ]( const size_t i ) {
-								if ( i > 0 ) {
-									foldl( v[ i ], alpha, divide );
-								}
-							},
-							v );
+					[ &v, &alpha, &divide ]( const size_t i ) {
+						if ( i > 0 ) {
+							internal::foldl( v[ i ], *alpha, divide );
+						}
+					},
+					v
+				);
 
 
 				// LL[ k+1: , k+1: ] -= v*v^T
 				auto LLprim = get_view( LL, utils::range( k + 1, n ), utils::range( k + 1, n ) );
 
-				vvt = outer( v, ring.getMultiplicativeOperator() );
+				auto vvt = outer( v, ring.getMultiplicativeOperator() );
+#ifdef TEMP_DISABLE
+				std::cout << "warning:  missing foldl( Mat, Mat, minus )";
+				std::function< void( typename operators::subtract< D > &, const size_t, const size_t ) > lambda_f_minus =
+					[ &LLprim, &vvt, &minus ]( typename operators::subtract< D >::D3 &result, const size_t i, const size_t j ) {
+						result = LLprim[ i, j ];
+						//result = minus( LLprim[ i, j ], vvt[ i, j ] );
+						//internal::apply( result, x[ i ], y[ j ], minus );
+					};
+#else
 				rc = foldl( LLprim, vvt, minus );
+#endif
 
 			}
 
@@ -96,8 +113,8 @@ namespace alp {
 			for( size_t k = 0; k < n ; ++k ) {
 
 				// L[ k: , k ] = LL[ k: , k ]
-				auto vL  = get_view( L  , utils::range( k, n) , k );
-				auto vLL = get_view( LL , utils::range( k, n) , k );
+				auto vL  = get_view( L  , utils::range( k, n), k  );
+				auto vLL = get_view( LL , utils::range( k, n), k  );
 
 				rc = set( vL, vLL );
 
