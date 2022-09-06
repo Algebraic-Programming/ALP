4cf432fa779abc3c92aa0fb852f6ce539936aba2
Albert-Jan Yzelman <albertjan.yzelman@huawei.com>
2022-09-06 16:07:14 +0200

    Correct license header, and documents the SCC Pregel algorithm

--- include/graphblas/algorithms/pregel_connected_components.hpp
+++ include/graphblas/algorithms/pregel_connected_components.hpp
@@ -1,9 +1,22 @@
 
 /*
- * Copyright Huawei Technologies Switzerland AG
- * All rights reserved.
+ *   Copyright 2021 Huawei Technologies Co., Ltd.
  *
- * @author A. N. Yzelman
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * @author: A. N. Yzelman.
  */
 
 #ifndef _H_GRB_PREGEL_CONNECTEDCOMPONENTS
@@ -18,11 +31,43 @@ namespace grb {
 
 		namespace pregel {
 
+			/**
+			 * A vertex-centric Strongly Connected Components algorithm.
+			 */
 			template< typename VertexIDType >
 			struct ConnectedComponents {
 
+				/**
+				 * This vertex-centric Strongly Connected Components algorithm does not
+				 * require any algorithm parameters.
+				 */
 				struct Data {};
 
+				/**
+				 * The vertex-centric program for computing the SCC. On termination, the
+				 * number of individual IDs in \a current_max_ID signifies the number of
+				 * components, while the value at each entry signifies which component
+				 * the vertex corresponds to.
+				 *
+				 * @param[in,out] current_max_ID On input: each entry is set to an unique
+				 *                               ID, corresponding to a unique ID for each
+				 *                               vertex. On output: the ID of the component
+				 *                               the corresponding vertex belongs to.
+				 * @param[in]   incoming_message A buffer for incoming messages to a vertex
+				 *                               program.
+				 * @param[in]   outgoing_message A buffer for outgoing messages to a vertex
+				 *                               program.
+				 * @param[in]         parameters Global algorithm parameters, currently an
+				 *                               instance of an empty struct (no
+				 *                               parameters).
+				 * @param[in,out]         pregel The Pregel state the program may refer to.
+				 *
+				 * This program 1) broadcasts its current ID to its neighbours, 2) checks
+				 * if any received IDs are larger than the current ID, then 3a) if not,
+				 * votes to halt; 3b) if yes, replaces the current ID with the received
+				 * maximum. It is meant to be executed using a max monoid as message
+				 * aggregator.
+				 */
 				static void program(
 					VertexIDType &current_max_ID,
 					const VertexIDType &incoming_message,
@@ -47,6 +92,25 @@ namespace grb {
 					}
 				}
 
+				/**
+				 * A convenience function that, given a Pregel instance, executes the SCC
+				 * #program.
+				 *
+				 * @param[in,out] pregel A Pregel instance over which to execute the SCC
+				 *                       program.
+				 * @param[out] group_ids The ID of the component the corresponding vertex
+				 *                       belongs to.
+				 * @param[in]  max_steps A maximum number of rounds the SCC program is
+				 *                       allowed to run. If \a 0, no maximum number of
+				 *                       rounds will be in effect.
+				 *
+				 * On succesful termination, the number of rounds is optionally written
+				 * out:
+
+				 * @param[out] steps_taken A pointer to where the number of rounds should
+				 *                         be recorded. Will not be used if equal to
+				 *                         <tt>nullptr</tt>.
+				 */
 				template< typename PregelType >
 				static grb::RC execute(
 					grb::interfaces::Pregel< PregelType > &pregel,
