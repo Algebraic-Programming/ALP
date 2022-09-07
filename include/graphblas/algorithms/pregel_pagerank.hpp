c8a792a2d0949baf6c99c1c3600b07a6a9f682a0
Albert-Jan Yzelman <albertjan.yzelman@huawei.com>
2022-09-07 15:37:28 +0200

    Add documentation

--- include/graphblas/algorithms/pregel_pagerank.hpp
+++ include/graphblas/algorithms/pregel_pagerank.hpp
@@ -18,14 +18,52 @@ namespace grb {
 
 		namespace pregel {
 
+			/**
+			 * A Pregel-style PageRank-like algorithm.
+			 *
+			 * This vertex-centric program does not correspond to the canonical PageRank
+			 * algorithm by Brin and Page. In particular, it misses corrections for
+			 * dangling nodes and does not perform convergence checks in any norm.
+			 *
+			 * @tparam IOType The type of the PageRank scores (e.g., <tt>double</tt>).
+			 * @tparam localConverge Whether vertices become inactive once their local
+			 *                       scores have converged, or whether to terminate only
+			 *                       when all vertices have converged.
+			 */
 			template< typename IOType, bool localConverge >
 			struct PageRank {
 
+				/**
+				 * The algorithm parameters.
+				 */
 				struct Data {
+
+					/**
+					 * The probability of jumping to a random page instead of a linked page.
+					 */
 					IOType alpha = 0.15;
+
+					/**
+					 * The local convergence criterion.
+					 */
 					IOType tolerance = 0.00001;
+
 				};
 
+				/**
+				 * The vertex-centric PageRank-like program.
+				 *
+				 * @param[out] current_score    The current rank corresponding to this
+				 *                              vertex.
+				 * @param[in]  incoming_message Neighbour contributions to our score.
+				 * @param[out] outgoing_message The score contribution to send to our
+				 *                              neighbours.
+				 * @param[in]     parameters    The algorithm parameters.
+				 * @param[in,out]     pregel    The state of the Pregel interface.
+				 *
+				 * The Pregel program expects incoming messages to be aggregated using a
+				 * plus monoid over elements of \a IOType.
+				 */
 				static void program(
 					IOType &current_score,
 					const IOType &incoming_message,
@@ -39,7 +77,9 @@ namespace grb {
 					}
 
 #ifdef _DEBUG
-					const bool dbg = pregel.vertexID == 17;
+					// when in debug mode, probably one does not wish to track the state of
+					// each vertex individually, hence we include a simple guard by default:
+					const bool dbg = pregel.vertexID == 0;
 					if( dbg ) {
 						std::cout << "ID: " << pregel.vertexID << "\n"
 							<< "\t active: " << pregel.active << "\n"
@@ -83,6 +123,42 @@ namespace grb {
 
 				}
 
+				/**
+				 * A convenience function for launching a PageRank algorithm over a given
+				 * Pregel instance.
+				 *
+				 * @tparam PregelType The nonzero type of an edge in the Pregel instance.
+				 *
+				 * This convenience function materialises the buffers expected to be passed
+				 * into the Pregel instance, and selects the expected monoid for executing
+				 * this program.
+				 *
+				 * \warning In performance-critical code, one may want to pre-allocate the
+				 *          buffers instead of having this convenience function allocate
+				 *          those. In such cases, please call manually the Pregel execute
+				 *          function, i.e., #grb::interfaces::Pregel< PregelType >::execute.
+				 *
+				 * The following arguments are mandatory:
+				 *
+				 * @param[in]  pregel      The Pregel instance that this program should
+				 *                         execute on.
+				 * @param[out] scores      A vector that corresponds to the scores
+				 *                         corresponding to each vertex. It must be of size
+				 *                         equal to the number of vertices \f$ n \f$ in the
+				 *                         \a pregel instance, and must have \f$ n \f$
+				 *                         capacity \em and values. The initial contents are
+				 *                         ignored by this algorithm.
+				 * @param[out] steps_taken How many rounds the program took until
+				 *                         termination.
+				 *
+				 * The following arguments are optional:
+				 *
+				 * @param[in] parameters The algorithm parameters. If not given, default
+				 *                       values will be substituted.
+				 * @param[in] max_steps  The maximum number of rounds this program may take.
+				 *                       If not given, the number of rounds will be
+				 *                       unlimited.
+				 */
 				template< typename PregelType >
 				static grb::RC execute(
 					grb::interfaces::Pregel< PregelType > &pregel,
