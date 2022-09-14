a76177937d7ec1e2cd96f37a827d931d5c944d00
Anonymous <anon@example.com>
2022-09-14 13:43:17 +0200

    523 dense file iterator (#70)
    Implementation of file reader and iterator for use in buildMatrix() and similar.
    
    This implementation reads only  dense matrices saved in Matrix Market Array Format.
    
    Currently only forward access iterator is supported. 
    Matrix Market Array Format stores elements in column-wise order and this iterator is returning elements in the same order.
    
    For matrices with symmetry, Symmetric and  Hermitian matrices are supported, 
    but iterator reads and returns only lower triangular part of matrix.
    It should be taken care, within ALP code, to build the proper matrix, from this lower triangular part, or implement the corresponding logic.
    
    Buffer for matrix elements has been removed, and should be implemented in the future.
    
    It is suggested that this utility should be merged with analogous reader for sparse matrices, implemented in GRB namespace. 
    
    
    Co-authored-by: Denis Jelovina <denis.jelovina@huawei.com>

--- tests/unit/alp_file_iterator.cpp
+++ tests/unit/alp_file_iterator.cpp
@@ -20,79 +20,57 @@
 #include <vector>
 
 #include <alp.hpp>
-#include <alp/algorithms/cholesky.hpp>
 #include <alp/utils/parser/MatrixFileReader.hpp>
 
 using namespace alp;
 
-void alp_program( const size_t & unit, alp::RC & rc ) {
+std::string fname;
+
+void alp_program( const size_t & in, alp::RC & rc ) {
+	(void)in;
 	rc = SUCCESS;
 
 	alp::utils::MatrixFileReader<
 		double
-	> parser_A( std::string("/home/d/Repos/graphblas/datasets/mymatrix.mtx") );
+	> parser_A( fname );
 
 	for ( auto it = parser_A.begin() ; it != parser_A.end() ; ++it  ) {
 		std::cout << " i,j,v= " << it.i() << " " << it.j() << " " << it.v() << "\n";
 	}
 
-	return ;
-
-
-
-	alp::Semiring< alp::operators::add< double >, alp::operators::mul< double >, alp::identities::zero, alp::identities::one > ring;
-
-	std::cout << "\tTesting ALP cholesky\n"
-	             "\tH = L * L^T\n";
-
-	// dimensions of sqare matrices H, L
-	size_t N = 10 * unit;
-
-	// dimensions of views over H, Q and R
-	size_t n = 2 * unit;
-
-	alp::Matrix< double, structures::SymmetricPositiveDefinite, Dense > H( N, N );
-	alp::Matrix< double, structures::LowerTriangular, Dense > L( N, N );
-
-	rc = algorithms::cholesky_lowtr( L, H, ring );
+	return;
 }
 
 int main( int argc, char ** argv ) {
 	// defaults
 	bool printUsage = false;
-	size_t in = 100;
 
 	// error checking
 	if( argc > 2 ) {
 		printUsage = true;
 	}
 	if( argc == 2 ) {
-		size_t read;
 		std::istringstream ss( argv[ 1 ] );
-		if( ! ( ss >> read ) ) {
+		if( ! ( ss >> fname ) ) {
 			std::cerr << "Error parsing first argument\n";
 			printUsage = true;
 		} else if( ! ss.eof() ) {
 			std::cerr << "Error parsing first argument\n";
 			printUsage = true;
-		} else if( read % 2 != 0 ) {
-			std::cerr << "Given value for n is odd\n";
-			printUsage = true;
 		} else {
 			// all OK
-			in = read;
 		}
 	}
 	if( printUsage ) {
-		std::cerr << "Usage: " << argv[ 0 ] << " [n]\n";
-		std::cerr << "  -n (optional, default is 100): an even integer, the "
-					 "test size.\n";
+		std::cerr << "Usage: " << argv[ 0 ] << " \n";
+		std::cerr << "  -filename \n";
 		return 1;
 	}
 
 	std::cout << "This is functional test " << argv[ 0 ] << "\n";
 	alp::Launcher< AUTOMATIC > launcher;
 	alp::RC out;
+	size_t in;
 	if( launcher.exec( &alp_program, in, out, true ) != SUCCESS ) {
 		std::cerr << "Launching test FAILED\n";
 		return 255;
