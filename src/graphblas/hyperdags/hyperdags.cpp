d0cf8848b0d56706e60d51526713d09dea4bd109
Albert-Jan N. Yzelman <albertjan.yzelman@huawei.com>
2022-10-04 14:03:02 +0200

    Richer _DEBUG mode output

--- src/graphblas/hyperdags/hyperdags.cpp
+++ src/graphblas/hyperdags/hyperdags.cpp
@@ -617,28 +617,37 @@ size_t grb::internal::hyperdags::HyperDAGGenerator::addAnySource(
 	const void * const pointer
 ) {
 #ifdef _DEBUG
-	std::cerr << "\t entering HyperDAGGen::addAnySource for " << pointer << "\n";
+	std::cerr << "\t entering HyperDAGGen::addAnySource for " << pointer << " and "
+		<< "type " << toString( type ) << ". Current sources:\n";
+	for( const auto &pair : sourceVertices ) {
+		std::cerr << "\t\t " << pair.first << "\n";
+	}
 #endif
 	const auto &find = sourceVertices.find( pointer );
 	if( find != sourceVertices.end() ) {
 #ifdef _DEBUG
 		std::cerr << "\t\t entry already existed, removing it\n";
 #endif
-		sourceVertices.erase( find );
 	}
+	assert( sourceVertices.find( pointer ) == sourceVertices.end() );
 	const size_t global_id = hypergraph.createVertex();
-	const auto &sourceVertex = sourceGen.create( type, global_id );
+	const auto sourceVertex = sourceGen.create( type, global_id );
 #ifdef _DEBUG
 	std::cerr << "\t\t created a source vertex with global ID " << global_id
-		<< " and local ID " << sourceVertex.getLocalID() << "\n";
+		<< " and local ID " << sourceVertex.getLocalID()
+		<< " that will be associated to the unique identifier "
+		<< pointer << "\n";
 #endif
 	assert( sourceVertex.getGlobalID() == global_id );
 	sourceVertices.insert( std::make_pair( pointer, sourceVertex ) );
 	sourceVec.push_back( sourceVertex );
 #ifdef _DEBUG
-	std::cerr << "\t\t sourceVertices and sourceVec sizes: "
+	std::cerr << "\t\t Sizes of sourceVertices and sourceVec: "
 		<< sourceVertices.size() << ", resp., "
-		<< sourceVec.size() << "\n";
+		<< sourceVec.size() << ".\n\t\t Contents of sourceVertices:\n";
+	for( const auto &pair : sourceVertices ) {
+		std::cerr << "\t\t\t " << pair.first << "\n";
+	}
 #endif
 	return global_id;
 }
