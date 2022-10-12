9d78cd8a2194340452012c50bf11743506736ecd
Aristeidis Mastoras <104904438+aristeidis-mastoras@users.noreply.github.com>
2022-10-12 18:10:16 +0200

    419 benchmarking spmv spmspv spmspm (#75)
    This MR adds three kernel-level performance tests to ALP/GraphBLAS: sparse matrix--dense vector multiplication, sparse matrix--sparse vector multiplication, and sparse matrix--sparse matrix multiplication. These three tests run on arbitrary input data sets, and employ the usual benchmarking methodology by default: run at least 10 experiments while computing the min, max, avg, and stddev aggregates and while sleeping for one second between experiments.
    Individual timings, again when used in default mode, will take at least 100 ms.-- if a single computation is shorter than that, then an "individual" timing in fact corresponds to n such timings, where n is the minimum number of times for which the computation takes 100 milliseconds or more. Both the resulting "inner" (100 ms.) and "outer" (10) repetitions may of course also be manually set.
    
    The performance test script is updated to run these three new performance tests on a fixed series of datasets. If one of the test sets is not available, then the experiment is skipped. For the sparse matrix--sparse matrix multiplication, square matrices are multiplied with itself; this test is, by default, only enabled for the smaller matrices in the dataset. The dataset series can easily be extended in the script should need be.
    
    The new performance tests are only run for the smallest datasets for the hyperdags backend, in-line with the current behaviour of performance tests for that backend already present in the main branch.
    
    The MR also includes minor code style fixes, as well as a workaround for CIs that run out of storage capacity due to the -g generating large binary files, while, related, not archiving some additional files that are not required to execute tests after building them.
    
    Thanks to Anders Hansson and Aristeidis Mastoras for contributing this MR!

--- tests/performance/performancetests.sh
+++ tests/performance/performancetests.sh
@@ -73,6 +73,9 @@ DATASET_SIZES=(497 4039 27770 334863 1134890 3774768 3072441)
 KNN4SOLS=(118 499 2805 1 64 1 1048907)
 KNN6SOLS=(357 547 5176 1 246 1 1453447)
 
+#the following datasets are used for benchmarking SpMV, SpMSpV, and SpMSpM
+MULTIPLICATION_DATASETS=(west0497.mtx fidap037.mtx cavity17.mtx s3rmt3m3.mtx bloweybq.mtx bcsstk17.mtx Pres_Poisson.mtx gyro_m.mtx memplus.mtx lhr34.mtx bcsstk32.mtx vanbody.mtx s3dkt3m2.mtx G2_circuit.mtx Stanford.mtx coPapersCiteseer.mtx bundle_adj.mtx Stanford_Berkeley.mtx apache2.mtx Emilia_923.mtx ldoor.mtx ecology2.mtx Serena.mtx cage14.mtx G3_circuit.mtx wikipedia-20051105.mtx wikipedia-20061104.mtx Freescale1.mtx wikipedia-20070206.mtx Queen_4147.mtx cage15.mtx adaptive.mtx rgg_n_2_24_s0.mtx uk-2002.mtx road_usa.mtx MOLIERE_2016.mtx europe_osm.mtx twitter.mtx com-Friendster.mtx)
+
 #which command to use to run a GraphBLAS program
 LPF=yes
 if [ -z "${LPFRUN}" ]; then
@@ -277,6 +280,81 @@ runOtherBenchMarkTests()
 	echo >> ${TEST_OUT_DIR}/benchmarks
 }
 
+runMultiplicationKernels()
+{
+	local runner=$1
+	local backend=$2
+	local dataSet=$3
+	local parseMode=$4
+	local i=$5
+
+	# the check for the matrices existence is assumed to have already passed
+
+	if [ -z "$EXPTYPE" ] || [ "$EXPTYPE" == "SPMV" ]; then
+
+		# ---------------------------------------------------------------------
+		# spmv
+		echo ">>>      [ ]           [x]       Testing spmv using ${dataSet} dataset, $backend backend."
+		echo
+		$runner ${TEST_BIN_DIR}/driver_spmv_${backend} ${INPUT_DIR}/${dataSet} ${parseMode} &> ${TEST_OUT_DIR}/driver_spmv_${backend}_${dataSet}
+		head -1 ${TEST_OUT_DIR}/driver_spmv_${backend}_${dataSet}
+		if grep -q "Test OK" ${TEST_OUT_DIR}/driver_spmv_${backend}_${dataSet}; then
+			printf "Test OK\n\n"
+		else
+			printf "Test FAILED\n\n"
+		fi
+		echo "$backend spmv using the ${dataSet} dataset" >> ${TEST_OUT_DIR}/benchmarks
+		egrep 'Avg|Std' ${TEST_OUT_DIR}/driver_spmv_${backend}_${dataSet} >> ${TEST_OUT_DIR}/benchmarks
+		echo >> ${TEST_OUT_DIR}/benchmarks
+
+	fi
+
+	if [ -z "$EXPTYPE" ] || [ "$EXPTYPE" == "SPMSPV" ]; then
+
+		# ---------------------------------------------------------------------
+		# spmspv
+		echo ">>>      [ ]           [x]       Testing spmspv using ${dataSet} dataset, $backend backend."
+		echo
+		$runner ${TEST_BIN_DIR}/driver_spmspv_${backend} ${INPUT_DIR}/${dataSet} ${parseMode} &> ${TEST_OUT_DIR}/driver_spmspv_${backend}_${dataSet}
+		head -1 ${TEST_OUT_DIR}/driver_spmspv_${backend}_${dataSet}
+		if grep -q "Test OK" ${TEST_OUT_DIR}/driver_spmspv_${backend}_${dataSet}; then
+			printf "Test OK\n\n"
+		else
+			printf "Test FAILED\n\n"
+		fi
+		echo "$backend spmspv using the ${dataSet} dataset" >> ${TEST_OUT_DIR}/benchmarks
+		egrep 'Avg|Std' ${TEST_OUT_DIR}/driver_spmspv_${backend}_${dataSet} >> ${TEST_OUT_DIR}/benchmarks
+		echo >> ${TEST_OUT_DIR}/benchmarks
+
+	fi
+
+	if [ -z "$EXPTYPE" ] || [ "$EXPTYPE" == "SPMSPM" ]; then
+
+		# ---------------------------------------------------------------------
+		# spmspm
+		echo ">>>      [ ]           [x]       Testing spmspm using ${dataSet} dataset, $backend backend."
+		echo
+		if [ "$BACKEND" = "bsp1d" ] || [ "$BACKEND" = "hybrid" ]; then
+			echo "Test DISABLED: no sparse level-3 operations recommended for 1D distributions."
+			echo " "
+		elif [ "$i" -gt "14" ]; then
+			echo "Tests DISABLED: by default, long-running sparse matrix--sparse matrix multiplications are disabled (skipping dataset ${dataSet})."
+			echo " "
+		else
+			$runner ${TEST_BIN_DIR}/driver_spmspm_${backend} ${INPUT_DIR}/${dataSet} ${INPUT_DIR}/${dataSet} ${parseMode} &> ${TEST_OUT_DIR}/driver_spmspm_${backend}_${dataSet}
+			head -1 ${TEST_OUT_DIR}/driver_spmspm_${backend}_${dataSet}
+			if grep -q "Test OK" ${TEST_OUT_DIR}/driver_spmspm_${backend}_${dataSet}; then
+				printf "Test OK\n\n"
+			else
+				printf "Test FAILED\n\n"
+			fi
+			echo "$backend spmspm using the ${dataSet} dataset" >> ${TEST_OUT_DIR}/benchmarks
+			egrep 'Avg|Std' ${TEST_OUT_DIR}/driver_spmspm_${backend}_${dataSet} >> ${TEST_OUT_DIR}/benchmarks
+			echo >> ${TEST_OUT_DIR}/benchmarks
+		fi
+	fi
+}
+
 # end helper functions
 
 if [ -z "$EXPTYPE" ] || ! [ "$EXPTYPE" == "KERNEL" ]; then
@@ -360,7 +438,8 @@ if [ -z "$EXPTYPE" ] || ! [ "$EXPTYPE" == "KERNEL" ]; then
 
 			# test for file
 			if [ ! -f ${INPUT_DIR}/${DATASET} ]; then
-				echo "Warning: dataset/${DATASET} not found. Provide the dataset to enable performance tests with it."
+				echo ">>>      [x]           [x]       Test algorithms using ${DATASET} dataset, ${BACKEND} backend."
+				echo "Tests DISABLED: dataset/${DATASET} not found. Provide the dataset to enable performance tests with it."
 				echo " "
 				continue
 			fi
@@ -394,6 +473,35 @@ if [ -z "$EXPTYPE" ] || ! [ "$EXPTYPE" == "KERNEL" ]; then
 				runOtherBenchMarkTests "$runner" "$BACKEND" "$DATASET" "$PARSE_MODE" 0 "simple_pagerank"
 
 			fi
+		done
+
+		for ((i=0;i<${#MULTIPLICATION_DATASETS[@]};++i));
+		do
+			if [ ! -z "$DATASETTORUN" ] && [ "$DATASETTORUN" != "${MULTIPLICATION_DATASETS[i]}" ]; then
+				continue
+			fi
+
+			if [ "$BACKEND" = "hyperdags" ] && [ "$i" -gt "0" ]; then
+				echo "Info: hyperdags performance tests run only on the smallest dataset"
+				echo " "
+				break
+			fi
+
+			# initialise parameters
+			DATASET=${MULTIPLICATION_DATASETS[i]}
+			PARSE_MODE=direct
+			
+			# test for file
+			if [ ! -f ${INPUT_DIR}/${DATASET} ]; then
+				echo ">>>      [ ]           [x]       Test multiplication kernels using ${DATASET} dataset,"
+				echo "                                 ${BACKEND} backend."
+				echo "Tests DISABLED: dataset/${DATASET} not found. Provide the dataset to enable performance tests with it."
+				echo " "
+				continue
+			fi
+
+			runMultiplicationKernels "$runner" "$BACKEND" "$DATASET" "$PARSE_MODE" "$i"
+
 		done
 
 	done
---
+++ tests/performance/spmspm.cpp
@@ -0,0 +1,408 @@
+
+/*
+ *   Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * @authors Anders Hansson, Aristeidis Mastoras, A. N. Yzelman
+ * @date May, 2022
+ */
+
+#include <exception>
+#include <iostream>
+#include <vector>
+
+#include <inttypes.h>
+
+#include <graphblas/utils/Timer.hpp>
+#include <graphblas/utils/parser.hpp>
+
+#include <graphblas.hpp>
+
+
+using namespace grb;
+
+struct input {
+	char filenameL[ 1024 ];
+	char filenameR[ 1024 ];
+	bool direct;
+	size_t rep;
+};
+
+struct output {
+	int error_code;
+	size_t rep;
+	grb::utils::TimerResults times;
+	PinnedVector< double > pinnedVector;
+	size_t result_nnz;
+};
+
+void grbProgram( const struct input &data_in, struct output &out ) {
+	// get user process ID
+	const size_t s = spmd<>::pid();
+	assert( s < spmd<>::nprocs() );
+
+	// get input n
+	grb::utils::Timer timer;
+	timer.reset();
+
+	// sanity checks on input
+	if( data_in.filenameL[ 0 ] == '\0' ) {
+		std::cerr << s << ": no file name given as input for left matrix." << std::endl;
+		out.error_code = ILLEGAL;
+		return;
+	} else if( data_in.filenameR[ 0 ] == '\n' ) {
+		std::cerr << s << ": no file name given as input for right matrix." << std::endl;
+		out.error_code = ILLEGAL;
+		return;
+	}
+
+	// assume successful run
+	out.error_code = 0;
+
+	// create local parser
+	grb::utils::MatrixFileReader< double,
+		std::conditional< (sizeof(grb::config::RowIndexType) >
+				sizeof(grb::config::ColIndexType)),
+			grb::config::RowIndexType,
+			grb::config::ColIndexType
+		>::type
+	> parserL( data_in.filenameL, data_in.direct );
+
+	grb::utils::MatrixFileReader< double,
+		std::conditional< (sizeof(grb::config::RowIndexType) >
+				sizeof(grb::config::ColIndexType)),
+			grb::config::RowIndexType,
+			grb::config::ColIndexType
+		>::type
+	> parserR( data_in.filenameR, data_in.direct );
+
+	assert( parserL.n() == parserR.m() );
+
+	const size_t l = parserL.m();
+	const size_t m = parserL.n();
+	const size_t n = parserR.n();
+
+	out.times.io = timer.time();
+	timer.reset();
+
+	// load into GraphBLAS
+	Matrix< double > A( l, m ), B( m, n );
+	{
+		RC rc = buildMatrixUnique(
+			A,
+			parserL.begin( SEQUENTIAL ), parserL.end( SEQUENTIAL ),
+			SEQUENTIAL
+		);
+		/* Once internal issue #342 is resolved this can be re-enabled
+		const RC rc = buildMatrixUnique( A,
+			parser.begin( PARALLEL ), parser.end( PARALLEL),
+			PARALLEL
+		);*/
+		if( rc != SUCCESS ) {
+			std::cerr << "Failure: call to buildMatrixUnique did not succeed for the "
+				<< "left-hand matrix " << "(" << toString( rc ) << ")." << std::endl;
+			out.error_code = 10;
+			return;
+		}
+
+		rc = buildMatrixUnique(
+			B,
+			parserR.begin( SEQUENTIAL ), parserR.end( SEQUENTIAL ),
+			SEQUENTIAL
+		);
+
+		if( rc != SUCCESS ) {
+			std::cerr << "Failure: call to buildMatrixUnique did not succeed for the "
+				<< "right-hand matrix " << "(" << toString( rc ) << ")." << std::endl;
+			out.error_code = 20;
+			return;
+		}
+	}
+
+	// check number of nonzeroes
+	try {
+		const size_t global_nnzL = nnz( A );
+		const size_t global_nnzR = nnz( B );
+		const size_t parser_nnzL = parserL.nz();
+		const size_t parser_nnzR = parserR.nz();
+		if( global_nnzL != parser_nnzL ) {
+			std::cerr << "Left matrix Failure: global nnz (" << global_nnzL << ") "
+				<< "does not equal parser nnz (" << parser_nnzL << ")." << std::endl;
+			return;
+		} else if( global_nnzR != parser_nnzR ) {
+			std::cerr << "Right matrix Failure: global nnz (" << global_nnzR << ") "
+				<< "does not equal parser nnz (" << parser_nnzR << ")." << std::endl;
+			return;
+		}
+
+	} catch( const std::runtime_error & ) {
+		std::cout << "Info: nonzero check skipped as the number of nonzeroes "
+			<< "cannot be derived from the matrix file header. The "
+			<< "grb::Matrix reports " << nnz( A ) << " nonzeroes in left "
+			<< "and " << nnz( B ) << " n right \n";
+	}
+
+	RC rc = SUCCESS;
+
+	// test default SpMSpM run
+	const Semiring<
+		grb::operators::add< double >, grb::operators::mul< double >,
+		grb::identities::zero, grb::identities::one
+	> ring;
+
+	// by default, copy input requested repetitions to output repititions performed
+	out.rep = data_in.rep;
+
+	// time a single call
+	{
+		Matrix< double > C( l, n );
+
+		grb::utils::Timer subtimer;
+		subtimer.reset();
+		rc = rc ? rc : grb::mxm( C, A, B, ring, RESIZE );
+		assert( rc == SUCCESS );
+		rc = rc ? rc : grb::mxm( C, A, B, ring );
+		assert( rc == SUCCESS );
+		double single_time = subtimer.time();
+
+		if( rc != SUCCESS ) {
+			std::cerr << "Failure: call to mxm did not succeed ("
+				<< toString( rc ) << ")." << std::endl;
+			out.error_code = 70;
+			return;
+		}
+		if( rc == SUCCESS ) {
+			rc = collectives<>::reduce( single_time, 0, operators::max< double >() );
+		}
+		if( rc != SUCCESS ) {
+			out.error_code = 80;
+			return;
+		}
+		out.times.useful = single_time;
+		const size_t deduced_inner_reps =
+			static_cast< size_t >( 100.0 / single_time ) + 1;
+		if( rc == SUCCESS && out.rep == 0 ) {
+			if( s == 0 ) {
+				std::cout << "Info: cold mxm completed"
+					<< ". Time taken was " << single_time << " ms. "
+					<< "Deduced inner repetitions parameter of " << out.rep << " "
+					<< "to take 1 second or more per inner benchmark.\n";
+				out.rep = deduced_inner_reps;
+			}
+			return;
+		}
+	}
+
+	if( out.rep > 1 ) {
+		std::cerr << "Error: more than 1 inner repetitions are not supported due to "
+			<< "having to time the symbolic phase while not timing the initial matrix "
+			<< "allocation cost\n";
+		out.error_code = 90;
+		return;
+	}
+
+	// allocate output for benchmark
+	Matrix< double > C( l, n );
+
+	// that was the preamble
+	out.times.preamble = timer.time();
+
+	// do benchmark
+	double time_taken;
+	timer.reset();
+
+#ifndef NDEBUG
+	rc = rc ? rc : grb::mxm( C, A, B, ring, RESIZE );
+	assert( rc == SUCCESS );
+	rc = rc ? rc : grb::mxm( C, A, B, ring );
+	assert( rc == SUCCESS );
+#else
+	(void) grb::mxm( C, A, B, ring, RESIZE );
+	(void) grb::mxm( C, A, B, ring );
+#endif
+
+	time_taken = timer.time();
+	if( rc == SUCCESS ) {
+		out.times.useful = time_taken / static_cast< double >( out.rep );
+	}
+	// print timing at root process
+	if( grb::spmd<>::pid() == 0 ) {
+		std::cout << "Time taken for a " << out.rep << " "
+			<< "mxm calls (hot start): " << out.times.useful << ". "
+			<< "Error code is " << out.error_code << std::endl;
+	}
+
+	// start postamble
+	timer.reset();
+
+	// set error code
+	if( rc == FAILED ) {
+		out.error_code = 30;
+		// no convergence, but will print output
+	} else if( rc != SUCCESS ) {
+		std::cerr << "Benchmark run returned error: " << toString( rc ) << "\n";
+		out.error_code = 35;
+		return;
+	}
+
+	// finish timing
+	time_taken = timer.time();
+	out.times.postamble = time_taken;
+
+	int nnz = 0;
+	auto it = C.begin();
+	while( it != C.end() ) {
+		if((*it).second != 0.0){
+			nnz++;
+		}
+		it.operator++();
+	}
+
+	out.result_nnz = nnz;
+
+	// done
+	return;
+}
+
+int main( int argc, char ** argv ) {
+	// sanity check
+	if( argc < 3 || argc > 7 ) {
+		std::cout << "Usage: " << argv[ 0 ] << " <datasetL> <datasetR> <direct/indirect> "
+			<< "(inner iterations) (outer iterations) (verification <truth-file>)\n";
+		std::cout << "<datasetL>, <datasetR>, and <direct/indirect> are mandatory arguments.\n";
+		std::cout << "<datasetL> is the left matrix of the multiplication and "
+			<< "<datasetR> is the right matrix \n";
+		std::cout << "(inner iterations) is optional, the default is "
+			<< grb::config::BENCHMARKING::inner() << ". "
+			<< "If set to zero, the program will select a number of iterations "
+			<< "approximately required to take at least one second to complete.\n";
+		std::cout << "(outer iterations) is optional, the default is "
+			<< grb::config::BENCHMARKING::outer() << ". "
+			<< "This value must be strictly larger than 0." << std::endl;
+		return 0;
+	}
+	std::cout << "Test executable: " << argv[ 0 ] << std::endl;
+#ifndef NDEBUG
+	std::cerr << "Warning: benchmark driver compiled without the NDEBUG macro "
+		<< "defined(!)\n";
+#endif
+
+	// the input struct
+	struct input in;
+
+	// get file name Left
+	(void)strncpy( in.filenameL, argv[ 1 ], 1023 );
+	in.filenameL[ 1023 ] = '\0';
+
+	// get file name Right
+	(void)strncpy( in.filenameR, argv[ 2 ], 1023 );
+	in.filenameL[ 1023 ] = '\0';
+
+	// get direct or indirect addressing
+	if( strncmp( argv[ 3 ], "direct", 6 ) == 0 ) {
+		in.direct = true;
+	} else {
+		in.direct = false;
+	}
+
+	// get inner number of iterations
+	in.rep = grb::config::BENCHMARKING::inner();
+	char * end = nullptr;
+	if( argc >= 5 ) {
+		in.rep = strtoumax( argv[ 4 ], &end, 10 );
+		if( argv[ 4 ] == end ) {
+			std::cerr << "Could not parse argument " << argv[ 3 ] << " "
+				<< "for number of inner experiment repititions." << std::endl;
+			return 2;
+		}
+	}
+
+	// get outer number of iterations
+	size_t outer = grb::config::BENCHMARKING::outer();
+	if( argc >= 6 ) {
+		outer = strtoumax( argv[ 5 ], &end, 10 );
+		if( argv[ 5 ] == end ) {
+			std::cerr << "Could not parse argument " << argv[ 4 ] << " "
+				<< "for number of outer experiment repititions." << std::endl;
+			return 4;
+		}
+	}
+
+	std::cout << "Executable called with parameters:  Left matrix A = "
+		<< in.filenameL << ", right matrix B = " << in.filenameR << ", "
+		<< "inner repititions = " << in.rep
+		<< ", and outer reptitions = " << outer
+		<< std::endl;
+
+	// the output struct
+	struct output out;
+
+	// set standard exit code
+	grb::RC rc = SUCCESS;
+
+	// launch estimator (if requested)
+	if( in.rep == 0 ) {
+		grb::Launcher< AUTOMATIC > launcher;
+		rc = launcher.exec( &grbProgram, in, out, true );
+		if( rc == SUCCESS ) {
+			in.rep = out.rep;
+		}
+		if( rc != SUCCESS ) {
+			std::cerr << "launcher.exec returns with non-SUCCESS error code "
+				<< (int)rc << std::endl;
+			return 6;
+		}
+	}
+
+	// launch benchmark
+	if( rc == SUCCESS ) {
+		grb::Benchmarker< AUTOMATIC > benchmarker;
+		rc = benchmarker.exec( &grbProgram, in, out, 1, outer, true );
+	}
+	if( rc != SUCCESS ) {
+		std::cerr << "benchmarker.exec returns with non-SUCCESS error code "
+			<< grb::toString( rc ) << std::endl;
+		return 8;
+	}
+
+	std::cout << "Error code is " << out.error_code << ".\n";
+
+	std::cout << "Number of non-zeroes in output matrix: "
+		<< out.result_nnz << "\n";
+
+	if( out.error_code == 0 && out.pinnedVector.size() > 0 ) {
+		std::cerr << std::fixed;
+		std::cerr << "Output matrix: (";
+		for( size_t k = 0; k < out.pinnedVector.nonzeroes(); k++ ) {
+			const auto &nonZeroValue = out.pinnedVector.getNonzeroValue( k );
+			std::cerr << nonZeroValue << ", ";
+		}
+		std::cerr << ")" << std::endl;
+		std::cerr << std::defaultfloat;
+	}
+
+	if( out.error_code != 0 ) {
+		std::cerr << std::flush;
+		std::cerr << "Test FAILED\n";
+	} else {
+		std::cout << "Test OK\n";
+	}
+	std::cout << std::endl;
+
+	// done
+	return out.error_code;
+}
+
