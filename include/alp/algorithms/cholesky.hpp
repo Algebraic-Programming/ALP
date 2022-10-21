f32c248e3c70dbbbc730bfa5e7f08d32f25c05a3
Denis Jelovina <denis.jelovina@huawei.com>
2022-10-21 13:48:44 +0200

    initial

--- include/alp/algorithms/cholesky.hpp
+++ include/alp/algorithms/cholesky.hpp
@@ -161,5 +161,99 @@ namespace alp {
 			return rc;
 		}
 
+
+#ifdef NOTNEABLES
+
+		/**
+		 * @brief Computes the blocked version Cholesky decomposition LL^H = H of a real symmetric
+		 *        or complex hermitian positive definite (SPD) matrix H where \a L is lower triangular.
+		 *        L^H  is equvalent to cojugate(transpose(L))
+		 *
+		 * @tparam D        Data element type
+		 * @tparam Ring     Type of the semiring used in the computation
+		 * @tparam Minus    Type minus operator used in the computation
+		 * @tparam Divide   Type of divide operator used in the computation
+		 * @param[out] L    output lower triangular matrix
+		 * @param[in]  H    input real symmetric (or complex hermitian) positive definite matrix
+		 * @param[in]  ring The semiring used in the computation
+		 * @return RC        SUCCESS if the execution was correct
+		 *
+		 */
+		template<
+			typename D,
+			typename Ring = Semiring< operators::add< D >, operators::mul< D >, identities::zero, identities::one >,
+			typename Minus = operators::subtract< D >,
+			typename Divide = operators::divide< D > >
+		RC cholesky_uptr_blk(
+			Matrix< D, structures::LowerTriangular, Dense > &L,
+			const Matrix< D, structures::HermitianPositiveDefinite, Dense > &H,
+			const size_t & bs,
+			const Ring & ring = Ring(),
+			const Minus & minus = Minus(),
+			const Divide & divide = Divide() ) {
+
+			RC rc = SUCCESS;
+
+			const size_t n = nrows( L );
+
+			Matrix< D, structures::HermitianPositiveDefinite, Dense > LL( n, n );
+			rc = set( LL, H );
+
+			//nb: number of blocks of (max) size bz
+			size_t nb = n / bs ;
+			if( n % bs != 0 ){
+				nb = nb + 1 ;
+			}
+
+			for( size_t i = 0; i < nb ; ++i ) {
+				//A11=L[i*bs:(i+1)*bs,i*bs:(i+1)*bs]
+				auto A11 = get_view(
+					LL,
+					utils::range( i * bs, std::max( ( i+1 ) * bs, n ) ),
+					utils::range( i * bs, std::max( ( i+1 ) * bs, n ) )
+				);
+				//A21=L[(i+1)*bs:,i*bs:(i+1)*bs]
+				auto A21 = get_view(
+					LL,
+					utils::range( i * bs, n ),
+					utils::range( i * bs, std::max( ( i+1 ) * bs, n ) )
+				);
+				//A22=L[(i+1)*bs:,(i+1)*bs:]
+				auto A22 = get_view(
+					LL,
+					utils::range( ( i + 1 ) * bs, n ),
+					utils::range( ( i + 1 ) * bs, n )
+				);
+
+				//A11=cholesky(A11)
+				Matrix< D, structures::LowerTriangular, Dense > tmpM(
+					std::min( bs, n - i * bs ),
+					std::min( bs, n - i * bs )
+				);
+				rc = cholesky_uptr( tmpM, A11 );
+				rc = set( A11, tmpM );
+
+				//A21=TRSM(A11,conjugate(A21).T)
+				auto A21ct = get_view<view::conjugate_transpose>( A21 );
+				rc = trsm( A11, A21ct );
+
+				//A22=A22-A21.dot(conjugate(A21).T)
+				auto A21A21H = kronecker( A21 );
+				rc = foldl( A22, A21A21H, minus );
+
+			}
+
+			for( size_t k = 0; k < n ; ++k ) {
+				// L[ k: , k ] = LL[ k: , k ]
+				auto vL  = get_view( L  , utils::range( k, n) , k );
+				auto vLL = get_view( LL , utils::range( k, n) , k );
+				rc = set( vL, vLL );
+			}
+
+			return rc;
+		}
+
+#endif
+
 	} // namespace algorithms
 } // namespace alp
