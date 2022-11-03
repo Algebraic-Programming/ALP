574779aaba4d2c92b426d52c1749fcf7ccab26f2
Denis Jelovina <denis.jelovina@huawei.com>
2022-11-03 17:21:20 +0100

    system-installed-blas linking

--- cmake/Blas.cmake
+++ cmake/Blas.cmake
@@ -1,4 +1,4 @@
-
+assert_valid_variables(INCLUDE_INSTALL_DIR)
 add_library(cblas INTERFACE)
 if(KBLAS_ROOT)
 	find_package(Kblas REQUIRED)
@@ -6,9 +6,28 @@ if(KBLAS_ROOT)
 	set(HEADER_NAME "kblas")
 else()
 	find_package(BLAS REQUIRED)
-	target_link_libraries(cblas INTERFACE BLAS::BLAS)
+        add_library( extBlas::extBlas UNKNOWN IMPORTED )
+        set_target_properties( extBlas::extBlas
+                PROPERTIES
+                IMPORTED_LOCATION "${BLAS_LIBRARIES}"
+		INTERFACE_LINK_OPTIONS "${BLAS_LINKER_FLAGS}"
+                #INTERFACE_INCLUDE_DIRECTORIES ${}
+        )
+
+	target_link_libraries(cblas INTERFACE extBlas::extBlas)
         set(HEADER_NAME "cblas")
 endif()
 
 file(WRITE "${CMAKE_BINARY_DIR}/blas_wrapper/blas.h" "#include \"${HEADER_NAME}.h\"\n" )
-target_include_directories(cblas INTERFACE "${CMAKE_BINARY_DIR}/blas_wrapper" )
+#target_include_directories(cblas INTERFACE "${CMAKE_BINARY_DIR}/blas_wrapper" )
+
+target_include_directories( cblas INTERFACE
+        $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/blas_wrapper>
+        $<INSTALL_INTERFACE:.>
+)
+install(FILES "${CMAKE_BINARY_DIR}/blas_wrapper/blas.h" DESTINATION "${INCLUDE_INSTALL_DIR}/blas_wrapper")
+
+install(
+	TARGETS cblas EXPORT GraphBLASTargets
+	INCLUDES DESTINATION "${INCLUDE_INSTALL_DIR}/blas_wrapper"
+)
