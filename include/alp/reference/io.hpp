a1fda9eeaa1522ee0482cd1eb4f28a165ccd7840
Vladimir Dimic <103253705+vdimiczrc@users.noreply.github.com>
2022-11-04 09:48:36 +0100

    582 remove getlength for alp vectors (#122)
    * Replace calls to getLength with calls to size
    * Move friend getLength to internal and refer to it from the public size function

--- include/alp/reference/io.hpp
+++ include/alp/reference/io.hpp
@@ -74,7 +74,7 @@ namespace alp {
 	size_t size(
 		const Vector< DataType, DataStructure, Density::Dense, View, ImfR, ImfC, reference > &x
 	) noexcept {
-		return getLength( x );
+		return internal::getLength( x );
 	}
 
 	/**
@@ -600,7 +600,7 @@ namespace alp {
 			return ILLEGAL;
 		}
 
-		if( getLength( x ) != getLength( y ) ) {
+		if( size( x ) != size( y ) ) {
 			return MISMATCH;
 		}
 
--- tests/unit/alp_type_traits.cpp
+++ tests/unit/alp_type_traits.cpp
@@ -78,9 +78,9 @@ void alp_program( const size_t & n, alp::RC & rc ) {
 	auto v_diag = alp::get_view< alp::view::diagonal >( M );
 	auto v_view1 = alp::get_view( v_diag );
 	auto v_view2 = alp::get_view( v_diag, alp::utils::range(1,2) );
-	std::cout << "v_diag( " << alp::getLength( v_diag ) << " )" << std::endl;
-	std::cout << "v_view1( " << alp::getLength( v_view1 ) << " )" << std::endl;
-	std::cout << "v_view2( " << alp::getLength( v_view2 ) << " )" << std::endl;
+	std::cout << "v_diag( " << alp::size( v_diag ) << " )" << std::endl;
+	std::cout << "v_view1( " << alp::size( v_view1 ) << " )" << std::endl;
+	std::cout << "v_view2( " << alp::size( v_view2 ) << " )" << std::endl;
 
 	ask_questions( v_diag, "v_diag" );
 	ask_questions( v_view1, "v_view1" );
