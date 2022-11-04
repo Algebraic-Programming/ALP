37032d49119d6facf516b74373107a617424aa84
Vladimir Dimic <vladimir.dimic@huawei.com>
2022-11-04 17:19:20 +0100

    Add unit test

---
+++ tests/unit/dense_omp_matrix.cpp
@@ -0,0 +1,96 @@
+
+/*
+ *   Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+#include <memory>
+
+#include <alp.hpp>
+
+void alp_program( const size_t &n, alp::RC &rc ) {
+
+	typedef double T;
+
+	rc = alp::SUCCESS;
+	alp::Semiring< alp::operators::add< T >, alp::operators::mul< T >, alp::identities::zero, alp::identities::one > ring;
+
+	std::cout << "\tStarting dense OMP matrix test with size: " << n << " x " << n <<  "\n";
+
+	// create the matrix
+	alp::Matrix< float, alp::structures::General > M( n, n );
+//	rc = rc ? rc : alp::set( M, alp::Scalar< T >( ring.template getOne< T >() ) );
+
+	// verify that accessing corner elements succeeds
+	// original matrix
+	//alp::internal::access( M, alp::internal::getStorageIndex( M, 0, 0 ) );
+	//alp::internal::access( M, alp::internal::getStorageIndex( M, height - 1, 0 ) );
+	//alp::internal::access( M, alp::internal::getStorageIndex( M, 0, width - 1 ) );
+	//alp::internal::access( M, alp::internal::getStorageIndex( M, height - 1, width - 1 ) );
+
+	rc = alp::SUCCESS;
+}
+
+int main( int argc, char ** argv ) {
+	// defaults
+	bool printUsage = false;
+	size_t in = 5;
+
+	// error checking
+	if( argc > 2 ) {
+		printUsage = true;
+	}
+	if( argc == 2 ) {
+		size_t read;
+		std::istringstream ss( argv[ 1 ] );
+		if( ! ( ss >> read ) ) {
+			std::cerr << "Error parsing first argument\n";
+			printUsage = true;
+		} else if( ! ss.eof() ) {
+			std::cerr << "Error parsing first argument\n";
+			printUsage = true;
+		} else if( read % 2 != 0 ) {
+			std::cerr << "Given value for n is odd\n";
+			printUsage = true;
+		} else {
+			// all OK
+			in = read;
+		}
+	}
+	if( printUsage ) {
+		std::cerr << "Usage: " << argv[ 0 ] << " [n]\n";
+		std::cerr << "  -n (optional, default is 100): an even integer, the "
+					 "test size.\n";
+		return 1;
+	}
+
+	std::cout << "This is functional test " << argv[ 0 ] << "\n";
+	alp::Launcher< alp::AUTOMATIC > launcher;
+	alp::RC out;
+	if( launcher.exec( &alp_program, in, out, true ) != alp::SUCCESS ) {
+		std::cerr << "Launching test FAILED\n";
+		return 255;
+	}
+	if( out != alp::SUCCESS ) {
+		std::cerr << "Test FAILED (" << alp::toString( out ) << ")" << std::endl;
+	} else {
+		std::cout << "Test OK" << std::endl;
+	}
+	return 0;
+}
