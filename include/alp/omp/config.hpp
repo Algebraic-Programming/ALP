55c63aa55762966597eae5fbaa21b24f388a0d7c
Daniele G. Spampinato <daniele.giuseppe.spampinato@huawei.com>
2022-12-05 18:39:50 +0100

    - Fixing mismatching new/delete bug - Enabling allocation/computation on sub-grid of threads -  Added unit tests for non-cubic mxm

--- include/alp/omp/config.hpp
+++ include/alp/omp/config.hpp
@@ -37,7 +37,7 @@ namespace alp {
 		constexpr size_t BLOCK_ROW_DIM = 2;
 		constexpr size_t BLOCK_COL_DIM = 2;
 
-		// Temporary solution to accomodate for 2.5D mxm
+		// Temporary squared solution to accomodate for 2.5D mxm
 		constexpr size_t THREAD_ROW_DIM = 4;
 		constexpr size_t THREAD_COL_DIM = 4;
 
