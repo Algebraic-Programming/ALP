a437acc8d3f0645a33f2d7cedc82d44e22ebcfea
Albert-Jan Yzelman <albertjan.yzelman@huawei.com>
2022-12-28 16:55:31 +0800

    Rework main documentation page. Two main TODOs remain: 1) write an ALP/Pregel intro, and 2) move / enable all documentation that should be public but is currently missed by the public doxygen configuration

--- include/graphblas.hpp
+++ include/graphblas.hpp
@@ -22,46 +22,149 @@
 
 /** \mainpage
  *
- * This is a GraphBLAS implementation in ANSI C++11. Authors:
- *   -# A. N. Yzelman, Huawei Technologies France; 2016-2020.
- *   -# A. N. Yzelman, Huawei Technologies Switzerland AG; 2020-current.
- *   -# Aristeidis Mastoras, Huawei Technologies Switzerland AG; 2020-current.
- *   -# Alberto Scolari, Huawei Technologies Switzerland AG; 2021-current.
- *   -# Verner Vlacic, Huawei Technologies Switzerland AG; 2021-current.
- *   -# Auke Booij, Huawei Technologies Switzerland AG; 2021.
- *   -# Dan Iorga, Huawei Technologies Switzerland AG; 2021.
- *   -# Daniel Di Nardo, Huawei Technologies France; 2017.
- *   -# Jonathan M. Nash, Huawei Technologies France; 2017.
+ * The Algebraic Programming (ALP) project is a modern and humble C++
+ * programming framework that achieves scalable and high performance.
  *
- * Contact: albertjan.yzelman@huawei.com
+ * With ALP, programmers are encouraged to express programs using algebraic
+ * concepts directly. ALP is a humble programming model in that it hides all
+ * optimisations pertaining to parallelisation, vectorisation, and other
+ * complexities with programming large-scale and heterogeneous systems.
  *
- * This API exposes only two containers: a #grb::Vector and a #grb::Matrix.
+ * ALP presently exposes the following interfaces:
+ *  -# generalised sparse linear algebra, \ref GraphBLAS;
+ *  -# vertex-centric programming, ALP/Pregel.
  *
- * All primitives defined on these containers must be given a (binary)
- * operator, a #grb::Monoid, or a #grb::Semiring. These monoid and semiring are
- * generalised from their mathematical counterpart in that they holds multiple
- * domains. The monoid consists of one binary operator and a corresponding
- * identity. The semiring consists of one additive operator, one multiplicative
- * operator, one identity under addition, and one identity under multiplication.
+ * Several other programming interfaces are under design at present.
+ *
+ * For authors who contributed to ALP, please see the NOTICE file.
+ *
+ * Contact:
+ *  - https://github.com/Algebraic-Programming/ALP
+ *  - https://gitee.com/CSL-ALP/graphblas/
+ *  - albertjan.yzelman@huawei.com
+ *
+ * @author A. N. Yzelman, Huawei Technologies France (2016-2020)
+ * @author A. N. Yzelman, Huawei Technologies Switzerland AG (2020-current)
+ *
+ * \defgroup GraphBLAS ALP/GraphBLAS
+ * @{
+ *
+ * @brief ALP/GraphBLAS enables sparse linear algebraic programming.
+ *
+ * \par API introduction
+ *
+ * ALP/GraphBLAS is an ANSI C++11 variant of the C GraphBLAS standard with a few
+ * different choices and an emphasis on portability and auto-parallelisation. It
+ * exposes only two containers: #grb::Vector and #grb::Matrix. A template
+ * argument controls the type of the values contained within a container.
+ *
+ * A container may have between \f$ 0 \f$ and \f$ c \f$ values, and each such
+ * value has a coordinate. The value \f$ c \f$ is the \em capacity of a
+ * container, and at most equals the \em size of that container. The size of a
+ * matrix is the product of its number of rows and its number of columns.
+ * Containers with fewer values than their size are considered \em sparse, while
+ * those with as many values as their size are considered \em dense. Scalars
+ * correspond to the standard C++ plain-old-data types, and, as such, have size,
+ * capacity, and number of values equal to one-- scalars are always dense. 
+ *
+ * For matrices, their size can be derived from #grb::nrows and #grb::ncols,
+ * while for vectors their size may be immediately retrieved via #grb::size.
+ * For both vectors and matrices, their capacity and current number of values
+ * may be retrieved via #grb::capacity and #grb::nnz, respectively. Finally,
+ * containers have a unique identifier that may be retrieved via #grb::getID.
+ * These identifiers are assigned in a deterministic fashion, so that for
+ * deterministic programs executed with the same number of processes, the same
+ * containers will be assigned the same IDs.
+ *
+ * Containers may be populated using #grb::set or by using dedicated I/O
+ * routines such as #grb::buildVectorUnique or #grb::buildMatrixUnique. Here,
+ * \em unique refers to the collection of values that should be ingested having
+ * no duplicate coordinates; i.e., there are no two values that map to the same
+ * coordinate. The first argument to either function is the output container,
+ * which is followed by an iterator pair that points to a collection of values
+ * to be ingested into the output container.
+ *
+ * ALP/GraphBLAS supports multiple user processes \f$ P \f$. If \f$ P > 1 \f$,
+ * there is a difference between #grb::SEQUENTIAL and #grb::PARALLEL I/O. The
+ * default I/O mode is #grb::PARALLEL, which may be overridden by supplying
+ * #grb::SEQUENTIAL as a fourth and final argument to the input routines. In
+ * sequential I/O, the iterator pair must point to the exact same collection
+ * of input values on each of the \f$ P \f$ user processes. In the parallel
+ * mode, however, each iterator pair points to disjoint value sets at each of
+ * the processes, while their union is what is logically ingested into the
+ * output container.
+ *
+ * Output iteration is done using the standard STL-style iterators. ALP,
+ * however, only supports const_iterators on output. Output iterators default
+ * to sequential mode also.
+ *
+ * Primitives perform algebraic operations on containers while using explicitly
+ * supplied algebraic structures. Primitives may be as simple as the
+ * element-wise application of a binary operator to two input vectors,
+ * generating values in a third output vector (\f$ z = x \odot y \f$,
+ * #grb::eWiseApply), or may be as rich as multiplying two matrices together
+ * whose result is to be added in-place to a third matrix
+ * (\f$ C \into C + AB \f$, #grb::mxm). The latter is typically deemed richer
+ * since it requires a semiring structure rather than a more basic binary
+ * operator.
+ *
+ * Primitives are grouped according to their classical BLAS levels:
+ *  - \ref BLAS0
+ *  - \ref BLAS1
+ *  - \ref BLAS2
+ *  - \ref BLAS3
+ *
+ * The "level-0" primitives operate on scalars, and in terms of arithmetic
+ * intensity match those of level-1 primitives-- however, since standard BLAS
+ * need not define scalar operations this specification groups them separately.
+ * All primitives except for #grb::set and #grb::eWiseApply are \em in-place,
+ * meaning that new output values are "added" to any pre-existing contents in
+ * output containers. The operator used for addition is derived from the
+ * algebraic structure that the primitive is called with.
+ *
+ * ALP/GraphBLAS defines three types of algebra structures, namely, a
+ *  -# binary operator such as #grb::operators::add (numerical addition),
+ *  -# #grb::Monoid, and
+ *  -# #grb::Semiring.
+ *
+ * Binary operators are parametrised in two input domains and one output domain,
+ * \f$ D_1 \times D_2 \to D_3 \f$. The \f$ D_i \f$ are given as template
+ * arguments to the operator. A #grb::monoid is composed from a binary operator
+ * coupled with an identity. For example, the additive monoid is defined as
+ * \code
+ *  grb::Monoid<
+ *    grb::operators::add< double >,
+ *    grb::identities::zero
+ *  >
+ * \endcode
+ * Note that passing a single domain as a template argument to a binary operator
+ * is a short-hand for an operator with \f$ D_{\{1,2,3\}} \f$ equal to the same
+ * domain.
+ *
+ * Likewise, a #grb::Semiring is composed from two monoids, where the first,
+ * the so-called additive monoid, furthermore must be commutative. The classic
+ * semiring over integers taught in elementary school, for example, reads
+ * \code
+ *  grb::Semiring<
+ *    grb::operators::add< unsigned int >,
+ *    grb::operators::mul< unsigned int >,
+ *    grb::identities::zero,
+ *    grb::identities::one
+ *  >
+ * \endcode
  *
  * Monoids and semirings must comply with their regular axioms-- a type system
  * assists users by checking for incorrect operators acting as additive or
- * multiplicative operators. Standard operators and identities are found in
- * their respective namespaces, #grb::operators and #grb::identities,
- * respectively.
+ * multiplicative monoids. Errors are reported <em>at compile time</em>, through
+ * the use of <em>algebraic type traits</em> such as #grb::is_associative.
  *
- * Monoids and semirings must be supplied with the domain(s) it will operate
- * on. These must be available at compile time. Also the element type of
- * GraphBLAS containers must be set at compile time. The size of a container is
- * set at run-time, but may not change during its life time.
+ * Standard operators and identities are found in their respective namespaces,
+ * #grb::operators and #grb::identities, respectively. The ALP monoids and
+ * semirings are generalised from their standard mathematical definitions in
+ * that they hold multiple domains. The description of #grb::Semiring details
+ * the underlying mathematical structure that nevertheless can be identified.
  *
- * This implementation provides various \ref BLAS1 and \ref BLAS2 primitives. To
- * simplify writing generalised algebraic routines, it also provides \ref BLAS0
- * primitives.
- *
- * The three aforementioned ingredients, namely, containers, algebraic relations
- * (such as semirings), and level-{1,2,3} primitives make up the full interface
- * of this DSL.
+ * \par ALP/GraphBLAS by example
  *
  * An example is provided within examples/sp.cpp. It demonstrates usage of this
  * API. We now follow with some code snippets from that example. First, the
@@ -105,26 +208,48 @@
  *
  * \snippet sp.cpp Example shortest-paths with semiring adapted to find the most reliable route instead
  *
- * Any GraphBLAS code may execute using any of the backends this implementation
- * defines. Currently, the following backends are stable:
+ * @author A. N. Yzelman, Huawei Technologies France (2016-2020)
+ * @author A. N. Yzelman, Huawei Technologies Switzerland AG (2020-current)
+ * @}
+ *
+ * \defgroup Backends ALP backends
+ * @{
+ *
+ * ALP code is compiled using a compiler wrapper, which optionally takes a
+ * backend parameter as an argument. The backend selection controls for which
+ * use case the code is compiled. Options that are always included are:
  *   -# #grb::reference, a single-process, auto-vectorising, sequential backend;
  *   -# #grb::reference_omp, a single-process, auto-parallelising, shared-memory
  *      parallel backend based on OpenMP and the aforementioned vectorising
  *      backend;
+ *   -# grb::hyperdags, a backend that captures the meta-data of computations
+ *      while delegating the actual work to the #grb::reference backend. At
+ *      program exit, the #grb::hyperdags backend dumps a HyperDAG of the
+ *      computations performed.
+ *
+ * Additionally, the following backends may be enabled by providing their
+ * dependences before building ALP:
  *   -# #grb::BSP1D, an auto-parallelising, distributed-memory parallel
  *      backend based on the Lightweight Parallel Foundations (LPF). This is a
  *      multi-process backend and may rely on any single-process backend for
- *      process-local computations. Its combination with the #grb::reference_omp
+ *      process-local computations, which by default is #grb::reference.
+ *      Distributed-memory auto-parallelisation is achieved using a row-wise
+ *      one-dimensional block-cyclic distributon.
+ *      Its combination with the #grb::reference_omp
  *      backend results in a fully hybrid shared- and distributed-memory
  *      GraphBLAS implementation.
- *
- * Backends that are currently under development:
+ *   -# #grb::hybrid, essentially the same backend as #grb::BSP1D, but now
+ *      composed with the #grb::reference_omp backend for process-local
+ *      computations. This backend facilitates full hybrid shared- and
+ *      distributed-memory parallelisation.
  *   -# #grb::banshee, a single-process, reference-based backend for the Banshee
  *      RISC-V hardware simulator making use of indirection stream semantic
- *      registers (ISSR, in collaboration with Prof. Benini at ETHZ);
+ *      registers (ISSR). Written by Dan Iorga in collaboration with ETHZ. This
+ *      backend is outdated, but, last tested, remained functional.
  *
  * @author A. N. Yzelman, Huawei Technologies France (2016-2020)
  * @author A. N. Yzelman, Huawei Technologies Switzerland AG (2020-current)
+ * @}
  */
 
 #ifdef __DOXYGEN__
