fee2ed922a599ff1d1712b0443bcc17d5ec413ff
Albert-Jan N. Yzelman <albertjan.yzelman@huawei.com>
2023-05-24 17:04:29 +0200

    Code review norm.hpp

--- include/graphblas/algorithms/norm.hpp
+++ include/graphblas/algorithms/norm.hpp
@@ -43,6 +43,20 @@ namespace grb {
 
 	namespace algorithms {
 
+		/**
+		 * An alias of std::sqrt where the input and output types are templated
+		 * separately.
+		 *
+		 * @tparam OutputType The output type of the square-root operation.
+		 * @tparam InputType The input type of the square-root operation.
+		 *
+		 * Relies on the standard std::sqrt implementation. If this is not available
+		 * for \a InputType, the use of this operation will result in a compile-time
+		 * error.
+		 *
+		 * This operation is used as a default to the #norm2 algorithm, as well as a
+		 * default to algorithms that depend on it.
+		 */
 		template< typename OutputType, typename InputType >
 		OutputType std_sqrt( InputType x ) {
 			return( static_cast< OutputType >( std::sqrt( x ) ) );
@@ -59,13 +73,13 @@ namespace grb {
 		 * For return codes, exception behaviour, performance semantics, template
 		 * and non-template arguments, @see grb::dot.
 		 *
-		 * @param[out] x The 2-norm of \a y. The input value of \a x will be ignored.
-		 * @param[in]  y The vector to compute the norm of.
+		 * @param[out]  x   The 2-norm of \a y. The input value of \a x will be
+		 *                  ignored.
+		 * @param[in]   y   The vector to compute the norm of.
 		 * @param[in] ring  The Semiring under which the 2-norm is to be computed.
 		 * @param[in] sqrtX The square root function which operates on real data
 		 *                  type, as both input and output. If not explicitly
 		 *                  provided, the std::sqrt() is used.
-		 *
 		 */
 		template<
 			Descriptor descr = descriptors::no_operation, class Ring,
@@ -75,12 +89,12 @@ namespace grb {
 		RC norm2( OutputType &x,
 			const Vector< InputType, backend, Coords > &y,
 			const Ring &ring = Ring(),
-			const std::function< OutputType( OutputType ) > sqrtX = std_sqrt< OutputType, OutputType >,
+			const std::function< OutputType( OutputType ) > sqrtX =
+				std_sqrt< OutputType, OutputType >,
 			const typename std::enable_if<
 				std::is_floating_point< OutputType >::value,
 			void >::type * = nullptr
 		) {
-
 			InputType yyt = ring.template getZero< InputType >();
 
 			RC ret = grb::dot< descr >(
@@ -100,7 +114,6 @@ namespace grb {
 			}
 			return ret;
 		}
-
 	}
 }
 
