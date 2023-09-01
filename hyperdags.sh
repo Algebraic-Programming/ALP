5565f42c9ce1e9a5bffeb97561bd90d0dd5ba7f4
Benjamin Lozes <benjamin.lozes@protonmail.com>
2023-09-01 10:17:44 +0200

    Adding dot unit-test

--- hyperdags.sh
+++ hyperdags.sh
@@ -9,6 +9,9 @@ fi
 mkdir -p "${HYPERDAGS_OUTPUT_FOLDER}/limited_iterations"
 mkdir -p "${HYPERDAGS_OUTPUT_FOLDER}/until_convergence"
 
+# Define paths
+SMOKE_TESTS_BIN_DIR="${TESTS_BIN_DIR}/smoke"
+UNIT_TESTS_BIN_DIR="${TESTS_BIN_DIR}/unit"
 
 # Build tests (if needed)
 make build_tests_category_smoke -j$(nproc)
@@ -18,58 +21,61 @@ make build_tests_category_smoke -j$(nproc)
 CURRENT_OUT_DIR="${HYPERDAGS_OUTPUT_FOLDER}/limited_iterations"
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/bicgstab.mtx" \
-    ${TEST_BIN_DIR}/bicgstab_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/bicgstab_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/conjugate_gradient.mtx" \
-    ${TEST_BIN_DIR}/conjugate_gradient_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/conjugate_gradient_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/pregel.mtx" \
-    ${TEST_BIN_DIR}/pregel_connected_components_hyperdags ${DATASETS_PATH}/west0497.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/pregel_connected_components_hyperdags ${DATASETS_PATH}/west0497.mtx direct 1 1
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/simple_pagerank.mtx" \
-    ${TEST_BIN_DIR}/simple_pagerank_hyperdags ${DATASETS_PATH}/west0497.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/simple_pagerank_hyperdags ${DATASETS_PATH}/west0497.mtx direct 1 1
 
 
 ## Until convergence tests
 CURRENT_OUT_DIR="${HYPERDAGS_OUTPUT_FOLDER}/until_convergence"
 
+HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/dot.mtx" \
+    ${UNIT_TESTS_BIN_DIR}/dot_hyperdags 1874
+
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/bicgstab_gyro_m.mtx" \
-    ${TEST_BIN_DIR}/bicgstab_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/bicgstab_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/conjugate_gradient_gyro_m.mtx" \
-    ${TEST_BIN_DIR}/conjugate_gradient_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/conjugate_gradient_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/hpcg.mtx" \
-    ${TEST_BIN_DIR}/hpcg
+    ${SMOKE_TESTS_BIN_DIR}/hpcg
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/k-NN_3_gyro_m.mtx" \
-    ${TEST_BIN_DIR}/knn_hyperdags 3 ${DATASETS_PATH}/gyro_m.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/knn_hyperdags 3 ${DATASETS_PATH}/gyro_m.mtx direct 1 1
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/k-means.mtx" \
-    ${TEST_BIN_DIR}/kmeans_hyperdags
+    ${SMOKE_TESTS_BIN_DIR}/kmeans_hyperdags
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/label_propagation_256.mtx" \
-    ${TEST_BIN_DIR}/labeltest_hyperdags 256
+    ${SMOKE_TESTS_BIN_DIR}/labeltest_hyperdags 256
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/label_propagation_4096.mtx" \
-    ${TEST_BIN_DIR}/labeltest_hyperdags 4096
+    ${SMOKE_TESTS_BIN_DIR}/labeltest_hyperdags 4096
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/pregel_connected_components_gyro_m.mtx" \
-    ${TEST_BIN_DIR}/pregel_connected_components_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/pregel_connected_components_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
 
 HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/simple_pagerank_gyro_m.mtx" \
-    ${TEST_BIN_DIR}/simple_pagerank_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/simple_pagerank_hyperdags ${DATASETS_PATH}/gyro_m.mtx direct 1 1
 
 if [ -f "${DATASETS_PATH}/wikipedia-20051105.mtx" ]; then
     HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/simple_pagerank_wikipedia-20051105.mtx" \
-    ${TEST_BIN_DIR}/simple_pagerank_hyperdags ${DATASETS_PATH}/wikipedia-20051105.mtx direct 1 1
+    ${SMOKE_TESTS_BIN_DIR}/simple_pagerank_hyperdags ${DATASETS_PATH}/wikipedia-20051105.mtx direct 1 1
 else
     echo "Skipping simple_pagerank_wikipedia-20051105.mtx test"
 fi
 
 if [ ! -z "${GNN_DATASET_PATH}" ] && [ -d "${GNN_DATASET_PATH}" ]; then
     HYPERDAGS_OUTPUT_PATH="${CURRENT_OUT_DIR}/graphchallenge_nn_single_inference_1024neurons_120layers.mtx" \
-        ${TEST_BIN_DIR}/graphchallenge_nn_single_inference_hyperdags ${GNN_DATASET_PATH} 1024 120 294 1 32 indirect 1 1
+        ${SMOKE_TESTS_BIN_DIR}/graphchallenge_nn_single_inference_hyperdags ${GNN_DATASET_PATH} 1024 120 294 1 32 indirect 1 1
 else
     echo "Skipping graphchallenge_nn_single_inference_1024neurons_120layers.mtx test"
 fi
