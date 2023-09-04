cabf350123912a34f62d754a324aee389f4ca312
Benjamin Lozes <benjamin.lozes@protonmail.com>
2023-09-04 18:09:17 +0200

    Random matrix generator - untested

--- include/graphblas/algorithms/matrix_factory.hpp
+++ include/graphblas/algorithms/matrix_factory.hpp
@@ -39,6 +39,8 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
+#include <algorithm>
 
 #include <graphblas.hpp>
 #include <graphblas/utils/iterators/regular.hpp>
@@ -724,6 +726,209 @@ namespace grb {
 		) {
 			static_assert( not std::is_void< D >::value, "factory::zeros can not be called with a void type" );
 			return full< D, descr, RIT, CIT, NIT, implementation >( nrows, ncols, io_mode, static_cast< D >(0) );
+		}
+
+		/**
+		 * @brief Build a matrix filled with random values at random positions.
+		 *
+		 * @tparam D                      The type of a non-zero values.
+		 * @tparam descr                  The descriptor used to build the matrix.
+		 * @tparam RIT                    The type used for row indices.
+		 * @tparam CIT                    The type used for column indices.
+		 * @tparam NIT                    The type used for non-zero indices.
+		 * @tparam RandomDeviceType       The type of the random device used to generate
+		 *                                the random data.
+		 * @tparam RowDistributionType    The type of the distribution used to generate
+		 *                                the row indices.
+		 * @tparam ColDistributionType    The type of the distribution used to generate
+		 *                                the column indices.
+		 * @tparam ValueDistributionType  The type of the distribution used to generate
+		 *                                the values.
+		 * @tparam implementation         The backend implementation used to build
+		 *                                the matrix (default: config::default_backend).
+		 *
+		 * @param nrows                   The number of rows of the matrix.
+		 * @param ncols                   The number of columns of the matrix.
+		 * @param io_mode                 The I/O mode used to build the matrix.
+		 * @param sparsity		          The sparsity factor of the matrix, 1.0 being
+		 *                                a dense matrix and 0.0 being an empty matrix.
+		 * @param rd                      The random device used to generate the random data.
+		 * @param row_dist                The distribution used to generate the row indices.
+		 * @param col_dist                The distribution used to generate the column indices.
+		 * @param val_dist                The distribution used to generate the values.
+		 * @return Matrix< D, implementation, RIT, CIT, NIT >
+		 */
+		template<
+			typename D,
+			Descriptor descr = descriptors::no_operation,
+			typename RIT = config::RowIndexType,
+			typename CIT = config::ColIndexType,
+			typename NIT = config::NonzeroIndexType,
+			typename RandomGeneratorType,
+			typename RowDistributionType,
+			typename ColDistributionType,
+			typename ValueDistributionType,
+			Backend implementation = grb::config::default_backend
+		>
+		Matrix< D, implementation, RIT, CIT, NIT > random(
+			const size_t nrows, const size_t ncols, IOMode io_mode,
+			double sparsity,
+			RandomGeneratorType &rgen,
+			RowDistributionType &row_dist,
+			ColDistributionType &col_dist,
+			ValueDistributionType &val_dist
+		) {
+			const size_t nvals = nrows * ncols * std::max( 1.0, std::min( 1.0, sparsity ) );
+			Matrix< D, implementation, RIT, CIT, NIT > matrix( nrows, ncols, nvals );
+
+			std::vector< RIT > I( nvals );
+			std::vector< CIT > J( nvals );
+			std::vector< D > V( nvals );
+			for( size_t i = 0; i < nvals; ++i ) {
+				I[ i ] = row_dist( rgen );
+				J[ i ] = col_dist( rgen );
+				V[ i ] = val_dist( rgen );
+			}
+
+			RC rc = ( descr & descriptors::transpose_matrix )
+				? buildMatrixUnique< descr >( matrix, J.begin(), J.end(), J.begin(), J.end(), V.begin(), V.end(), io_mode )
+				: buildMatrixUnique< descr >( matrix, I.begin(), I.end(), J.begin(), J.end(), V.begin(), V.end(), io_mode );
+
+			assert( rc == SUCCESS );
+			if( rc != SUCCESS ) {
+				// Todo: Throw an exception or just return an empty matrix?
+				// Nb: We should consider the distributed case if we throw an exception.
+				throw std::runtime_error(
+					"Error: factory::random failed: rc = " + grb::toString( rc )
+				);
+			}
+
+			return matrix;
+		}
+
+		template<
+			typename D,
+			Descriptor descr = descriptors::no_operation,
+			typename RIT = config::RowIndexType,
+			typename CIT = config::ColIndexType,
+			typename NIT = config::NonzeroIndexType,
+			typename RandomGeneratorType,
+			typename RowDistributionType,
+			typename ColDistributionType,
+			Backend implementation = grb::config::default_backend
+		>
+		Matrix< void, implementation, RIT, CIT, NIT > random(
+			const size_t nrows, const size_t ncols, IOMode io_mode,
+			double sparsity,
+			RandomGeneratorType &rgen,
+			RowDistributionType &row_dist,
+			ColDistributionType &col_dist,
+			typename std::enable_if< std::is_void< D >::value, int >::type = 0
+		) {
+			const size_t nvals = nrows * ncols * std::max( 1.0, std::min( 1.0, sparsity ) );
+			Matrix< void, implementation, RIT, CIT, NIT > matrix( nrows, ncols, nvals );
+
+			std::vector< RIT > I( nvals );
+			std::vector< CIT > J( nvals );
+			for( size_t i = 0; i < nvals; ++i ) {
+				I[ i ] = row_dist( rgen );
+				J[ i ] = col_dist( rgen );
+			}
+
+			RC rc = ( descr & descriptors::transpose_matrix )
+				? buildMatrixUnique< descr >( matrix, J.begin(), J.end(), J.begin(), J.end(), io_mode )
+				: buildMatrixUnique< descr >( matrix, I.begin(), I.end(), J.begin(), J.end(), io_mode );
+
+			assert( rc == SUCCESS );
+			if( rc != SUCCESS ) {
+				// Todo: Throw an exception or just return an empty matrix?
+				// Nb: We should consider the distributed case if we throw an exception.
+				throw std::runtime_error(
+					"Error: factory::random<void> failed: rc = " + grb::toString( rc )
+				);
+			}
+
+			return matrix;
+		}
+
+
+		/**
+		 * Build a matrix filled with random values at random positions.
+		 *
+		 * Will use an \a mt19937 random generator with the given seed.
+		 * The distributions used to generate the random data are uniform_real_distribution
+		 * with the following ranges:
+		 * * row indices:    [0, nrows - 1]
+		 * * column indices: [0, ncols - 1]
+		 * * values:         [0, 1]
+		 *
+		 *
+		 *
+		 * @tparam D              The type of a non-zero values.
+		 * @tparam descr          The descriptor used to build the matrix.
+		 * @tparam RIT            The type used for row indices.
+		 * @tparam CIT            The type used for column indices.
+		 * @tparam NIT            The type used for non-zero indices.
+		 * @tparam implementation The backend implementation used to build
+		 *                        the matrix (default: config::default_backend).
+		 * @param nrows           The number of rows of the matrix.
+		 * @param ncols           The number of columns of the matrix.
+		 * @param io_mode         The I/O mode used to build the matrix.
+		 * @param sparsity		  The sparsity factor of the matrix, 1.0 being
+		 *                        a dense matrix and 0.0 being an empty matrix.
+		 * @param seed            The seed used to generate the random values.
+		 * @return Matrix< D, implementation, RIT, CIT, NIT >
+		 */
+		template<
+			typename D,
+			Descriptor descr = descriptors::no_operation,
+			typename RIT = config::RowIndexType,
+			typename CIT = config::ColIndexType,
+			typename NIT = config::NonzeroIndexType,
+			Backend implementation = grb::config::default_backend
+		>
+		Matrix< D, implementation, RIT, CIT, NIT > random(
+			const size_t nrows, const size_t ncols, IOMode io_mode,
+			double sparsity,
+			const unsigned long seed = 0UL
+		) {
+			std::mt19937 gen( seed );
+
+			return random<
+				D, descr, RIT, CIT, NIT, implementation
+			>(
+				nrows, ncols, io_mode, sparsity,
+				gen,
+				std::uniform_real_distribution< RIT >( static_cast< RIT >( 0 ), nrows - 1 ),
+				std::uniform_real_distribution< CIT >( static_cast< CIT >( 0 ), ncols - 1 ),
+				std::uniform_real_distribution< D >( static_cast< D >( 0 ), static_cast< D >( 1 ) )
+			);
+		}
+
+		template<
+			typename D,
+			Descriptor descr = descriptors::no_operation,
+			typename RIT = config::RowIndexType,
+			typename CIT = config::ColIndexType,
+			typename NIT = config::NonzeroIndexType,
+			Backend implementation = grb::config::default_backend
+		>
+		Matrix< void, implementation, RIT, CIT, NIT > random(
+			const size_t nrows, const size_t ncols, IOMode io_mode,
+			double sparsity,
+			const unsigned long seed = 0UL,
+			typename std::enable_if< std::is_void< D >::value, int >::type = 0
+		) {
+			std::mt19937 gen( seed );
+
+			return random<
+				void, descr, RIT, CIT, NIT, implementation
+			>(
+				nrows, ncols, io_mode, sparsity,
+				gen,
+				std::uniform_real_distribution< RIT >( static_cast< RIT >( 0 ), nrows - 1 ),
+				std::uniform_real_distribution< CIT >( static_cast< CIT >( 0 ), ncols - 1 )
+			);
 		}
 
 	} // namespace factory
