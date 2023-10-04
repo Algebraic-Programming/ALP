b5bdbda1419f487e281007a8d3057d9c8a4606c9
Albert-Jan N. Yzelman <albertjan.yzelman@huawei.com>
2023-10-04 15:02:14 +0200

    Bugfixes and minor restructuring

--- include/graphblas/utils/iterators/filter.hpp
+++ include/graphblas/utils/iterators/filter.hpp
@@ -50,17 +50,43 @@ namespace grb {
 			 * the #IteratorFilter::create API.
 			 */
 			template< typename FwdSubIter >
+			class IteratorFilter;
+
+			/**
+			 * The factory function that takes any iterator pair over the same
+			 * container and returns a matching filtered iterator pair.
+			 *
+			 * @param[in] begin A sub iterator in begin position.
+			 * @param[in] end   A sub iterator that matches the end position of
+			 *                  \a begin.
+			 * @param[in] func  Values between \a begin and \a end for which this
+			 *                  function evaluates <tt>true</tt> shall \em not be
+			 *                  iterated over by the returned IteratorFilter instances.
+			 *
+			 * returns A pair of IteratorFilter instances, the first matching \a begin
+			 *         and the second matching \a end.
+			 *
+			 * \note If all elements from \a begin to \a end in the underlying
+			 *       collection are to be filtered according to \a func, then two
+			 *       equal IteratorFilter instances may be returned, indicative of the
+			 *       resulting empty set.
+			 */
+			template< typename FwdSubIter >
+			std::pair<
+				IteratorFilter< FwdSubIter >, IteratorFilter< FwdSubIter >
+			> createIteratorFilter(
+				FwdSubIter begin, const FwdSubIter end,
+				const std::function< bool(const typename FwdSubIter::value_type &) > func
+			);
+
+			template< typename FwdSubIter >
 			class IteratorFilter {
 
 				static_assert( std::is_base_of<
 						std::forward_iterator_tag,
-						std::iterator_traits< FwdSubIter >::iterator_category
+						typename std::iterator_traits< FwdSubIter >::iterator_category
 					>::value,
 					"The sub-iterator to IteratorFilter must be a forward iterator."
-				);
-
-				friend void swap(
-					IteratorFilter< FwdSubIter > &left, IteratorFilter< FwdSubIter > &right
 				);
 
 
@@ -69,20 +95,16 @@ namespace grb {
 					/** The value type of the underlying iterator. */
 					typedef typename std::iterator_traits< FwdSubIter >::value_type ValT;
 
-					/** The type of the user-specified filter function. */
+					/** The type of an user-specified filter function. */
 					typedef typename std::function< bool( const ValT & ) > FilterT;
-
-					/**
-					 * The return type of the factory function that creates IteratorFilter
-					 * instances.
-					 */
-					typedef std::pair<
-						IteratorFilter< FwdSubIter >,
-						IteratorFilter< FwdSubIter >
-					> FactoryOutputT;
 
 
 				private:
+
+					/**
+					 * Short-cut to our own type.
+					 */
+					typedef IteratorFilter< FwdSubIter > self_type;
 
 					/**
 					 * The underlying iterator.
@@ -118,40 +140,43 @@ namespace grb {
 
 				public:
 
+					// friend functions
+
 					/**
-					 * The factory function that takes any iterator pair over the same
-					 * container and returns a matching filtered iterator pair.
+					 * Swap two instances of the #grb::utils::iterators::IteratorFilter type.
 					 *
-					 * @param[in] begin A sub iterator in begin position.
-					 * @param[in] end   A sub iterator that matches the end position of
-					 *                  \a begin.
-					 * @param[in] func  Values between \a begin and \a end for which this
-					 *                  function evaluates <tt>true</tt> shall \em not be
-					 *                  iterated over by the returned IteratorFilter instances.
+					 * @tparam FwdSubIter The underlying iterator type.
 					 *
-					 * returns A pair of IteratorFilter instances, the first matching \a begin
-					 *         and the second matching \a end.
+					 * @param[in] left  The left-hand side input.
+					 * @param[in] right The right-hand side input.
 					 *
-					 * \note If all elements from \a begin to \a end in the underlying
-					 *       collection are to be filtered according to \a func, then two
-					 *       equal IteratorFilter instances may be returned, indicative of the
-					 *       resulting empty set.
+					 * This function will swap the contents of \a left with that of \a right.
 					 */
-					static FactoryOutputT create(
-						FwdSubIter begin, const FwdSubIter end, const FilterT func
-					) {
-						FactoryOutputT ret = {
-							IteratorFilter( begin, end, func ),
-							IteratorFilter( end, end, func )
-						};
-						if( begin != end ) {
-							while( begin != end && !func( *begin ) ) {
-								(void) ++begin;
-							}
-							ret.first = IteratorFilter( begin, end, func );
-						}
-						return ret;
+					friend void swap(
+						IteratorFilter< FwdSubIter > &left, IteratorFilter< FwdSubIter > &right
+					) noexcept {
+						std::swap( left.it, right.it );
+						std::swap( left.end, right.end );
+						std::swap( left.filter, right.filter );
 					}
+
+					template< typename T >
+					friend std::pair<
+						IteratorFilter< T >, IteratorFilter< T >
+					> createIteratorFilter(
+						T begin, const T end,
+						const std::function< bool(const typename T::value_type &) > func
+					);
+
+					// STL typedefs
+
+					typedef typename std::iterator_traits< self_type >::difference_type
+						difference_type;
+					typedef typename std::iterator_traits< self_type >::value_type value_type;
+					typedef typename std::iterator_traits< self_type >::pointer pointer;
+					typedef typename std::iterator_traits< self_type >::reference reference;
+					typedef typename std::iterator_traits< self_type >::iterator_category
+						iterator_category;
 
 					/**
 					 * An IteratorFilter may never be default-constructed.
@@ -219,15 +244,16 @@ namespace grb {
 					 * \warning Hence, just as in the STL spec, calling this iterator on an
 					 *          iterator instance in end position invites undefined behaviour.
 					 *
-					 * @returns A reference to this iterator, reflecting the state of this
-					 *          instance after the increment operation has completed.
+					 * @returns Another instance of this iterator, reflecting the state of this
+					 *          instance before the increment operation had completed.
 					 */
-					IteratorFilter< FwdSubIter >& operator++(int) {
+					IteratorFilter< FwdSubIter > operator++(int) {
+						IteratorFilter< FwdSubIter > ret( *this );
 						(void) it++;
 						while( it != end && filter( *it ) ) {
 							(void) it++;
 						}
-						return *this;
+						return ret;
 					}
 
 					/**
@@ -286,57 +312,66 @@ namespace grb {
 
 			};
 
-			/**
-			 * Iterator traits for the IteratorFilter.
-			 *
-			 * @tparam FwdSubIter The underlying iterator type.
-			 *
-			 * Inherits all values from the \a FwdSubIter, except for the iterator
-			 * category tag, which shall be reduced to a forward iterator.
-			 */
 			template< typename FwdSubIter >
-			struct std::iterator_traits< IteratorFilter< FwdSubIter > > {
-
-				/** This trait is inherited from \a FwdSubIter. */
-				typedef std::iterator_traits< FwdSubIter >::difference_type difference_type;
-
-				/** This trait is inherited from \a FwdSubIter. */
-				typedef std::iterator_traits< FwdSubIter >::value_type value_type;
-
-				/** This trait is inherited from \a FwdSubIter. */
-				typedef std::iterator_traits< FwdSubIter >::pointer pointer;
-
-				/** This trait is inherited from \a FwdSubIter. */
-				typedef std::iterator_traits< FwdSubIter >::reference reference;
-
-				/** This trait is always forward iterator. */
-				typedef std::forward_iterator_tag iterator_category;
-
-			};
-
-			/**
-			 * Swap two instances of the #grb::utils::iterators::IteratorFilter type.
-			 *
-			 * @tparam FwdSubIter The underlying iterator type.
-			 *
-			 * @param[in] left  The left-hand side input.
-			 * @param[in] right The right-hand side input.
-			 *
-			 * This function will swap the contents of \a left with that of \a right.
-			 */
-			template< typename FwdSubIter >
-			void swap(
-				IteratorFilter< FwdSubIter > &left, IteratorFilter< FwdSubIter > &right
-			) noexcept {
-				using std::swap;
-				swap( left.it, right.it );
-				swap( left.end, right.end );
-				swap( left.filter, right.filter );
+			std::pair<
+				IteratorFilter< FwdSubIter >, IteratorFilter< FwdSubIter >
+			> createIteratorFilter(
+				FwdSubIter begin, const FwdSubIter end,
+				const std::function< bool(const typename FwdSubIter::value_type &) > func
+			) {
+				std::pair <
+					IteratorFilter< FwdSubIter >, IteratorFilter< FwdSubIter >
+				> ret = {
+					IteratorFilter< FwdSubIter >( begin, end, func ),
+					IteratorFilter< FwdSubIter >( end, end, func )
+				};
+				if( begin != end ) {
+					while( begin != end && func( *begin ) ) {
+						(void) ++begin;
+					}
+					ret.first = IteratorFilter< FwdSubIter >( begin, end, func );
+				}
+				return ret;
 			}
 
-		}
+		} // end grb::utils::iterators namespace
 
-	}
+	} // end grb::utils namespace
+
+} // end grb namespace
+
+namespace std {
+
+	/**
+	 * Iterator traits for the IteratorFilter.
+	 *
+	 * @tparam FwdSubIter The underlying iterator type.
+	 *
+	 * Inherits all values from the \a FwdSubIter, except for the iterator
+	 * category tag, which shall be reduced to a forward iterator.
+	 */
+	template< typename FwdSubIter >
+	struct iterator_traits<
+		grb::utils::iterators::IteratorFilter< FwdSubIter >
+	> {
+
+		/** This trait is inherited from \a FwdSubIter. */
+		typedef typename std::iterator_traits< FwdSubIter >::difference_type
+			difference_type;
+
+		/** This trait is inherited from \a FwdSubIter. */
+		typedef typename std::iterator_traits< FwdSubIter >::value_type value_type;
+
+		/** This trait is inherited from \a FwdSubIter. */
+		typedef typename std::iterator_traits< FwdSubIter >::pointer pointer;
+
+		/** This trait is inherited from \a FwdSubIter. */
+		typedef typename std::iterator_traits< FwdSubIter >::reference reference;
+
+		/** This trait is always forward iterator. */
+		typedef typename std::forward_iterator_tag iterator_category;
+
+	};
 
 }
 
