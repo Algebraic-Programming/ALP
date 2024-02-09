ccaba4d5425e9a4601abe0fd07f820194d69de9b
Aleksa Milisavljevic <aleksamilis@gmail.com>
2024-02-09 11:21:52 +0100

    code cleanup

--- tests/unit/id.cpp
+++ tests/unit/id.cpp
@@ -220,7 +220,7 @@ void grb_program2( const struct input &in, struct output &out ) {
 	
 	Creating and performing move assignment on multiple new objects to generate potential id collision.
 	*/
-	for(int i=0;i<1000;i++) {
+	for( int i = 0; i < 1000; i++ ) {
 		grb::Matrix< int > new_one( 10, 10, 10 );
 		grb::Matrix< int > new_two( 10, 10, 10 );
 		new_two = std::move(new_one);
