9e2d3619a79f007bd2069a336b4057e985f7c202
Albert-Jan Yzelman <albertjan.yzelman@huawei.com>
2024-03-14 15:45:29 +0100

    Remove now unused function

--- include/graphblas/reference/compressed_storage.hpp
+++ include/graphblas/reference/compressed_storage.hpp
@@ -510,43 +510,6 @@ namespace grb {
 					return 2 * nz + m + 1;
 				}
 
-#if 0 // TODO: remove if indeed unused
-				template< typename ValueType >
-				void assignValue(
-					const size_t nz, const size_t,
-					const size_t start, size_t end,
-					const ValueType& val
-				) {
-					// The value can not be of type void
-					static_assert(
-						!std::is_void< ValueType >::value,
-						"ValueType must not be void. This is an internal error; please submit a "
-						"bug report"
-					);
-					// The value must be convertible to D
-					static_assert(
-						std::is_convertible< ValueType, D >::value,
-						"ValueType must be convertible to D. This is an internal error; please "
-						"submit a bug report"
-					);
-#ifdef _DEBUG
-					std::cout << "CompressedStorage::assignValue called with range "
-						<< start << "--" << end << ". Value " << val << " will be used.\n";
-#endif
-					if( start >= nz ) { return; }
-
-					const size_t loop_end = std::min( nz, end );
-#ifdef _DEBUG
-					std::cout << "\t value range " << start << " -- " << loop_end << "\n";
-#endif
-					assert( start <= loop_end );
-
-					GRB_UTIL_IGNORE_CLASS_MEMACCESS;
-					std::fill_n( values + start, loop_end - start, static_cast< D >( val ) );
-					GRB_UTIL_RESTORE_WARNINGS;
-				}
-#endif
-
 				/**
 				 * Copies contents from a given Compressed_Storage.
 				 *
