98e332a7107a135bd6c07b0be06ddf2d6eb4aa53
Daniele G. Spampinato <daniele.giuseppe.spampinato@huawei.com>
2024-04-19 14:22:09 +0200

    # This is a combination of 14 commits. # This is the 1st commit message:
    Issue #303: framework for reference_dense added
    
    This commit comes from a split of the original commit whose details are as follows:
    SHA1 ID: a7c1db0eb45326184dadec39a6a7731eb16a39fd
    Author: Albert-Jan Yzelman <yzelman@linux.fritz.box>  2022-01-14 15:52:26
    Committer: Albert-Jan Yzelman <yzelman@linux.fritz.box>  2022-01-30 13:48:35
    
    The reason for splitting is to separate the changes irrelevant to the branch 303 into a separate branch.
    The split produced 3 separate commits.
    This commit is number 1.
    This commit in particular is relevant to 303 and will remain in it.
    
    # This is the commit message #2:
    
    Issue #303: unify script argument similar to --no-reference
    
    # This is the commit message #3:
    
    Issue #303: complete implementation of Vector< reference_dense >, modulo output iteration.
    
    # This is the commit message #4:
    
    Issue #303: modified vector implementation according to option 3 in handling dense container construction.
    
    # This is the commit message #5:
    
    Issue #303: vectors now have an iterator for the user to extract data from.
    
    # This is the commit message #6:
    
    Issue #303, fixed silly oops: the difference between two iterators should be an integer
    
    # This is the commit message #7:
    
    Issue #303: containers are now uninitialized on construction. Iteration over uninitialized containers returns nothing.
    
    # This is the commit message #8:
    
    Issue #303: configure and cmake now properly do make install with reference_dense
    
    # This is the commit message #9:
    
    Issue #303: cmake infra has updated-- make dense_reference additions compatible to it
    
    # This is the commit message #10:
    
    Drafting structured matrix interface using reference_dense backend
    
    # This is the commit message #11:
    
    Issue #303: Copy test mxm.cpp to dense_mxm.cpp as a starting point
    
    # This is the commit message #12:
    
    Issue #303: Add partial first version of Matrix interface for denseref
    
    # This is the commit message #13:
    
    Issue #303: WIP: dense mxm test
    
    # This is the commit message #14:
    
    Issue #303: Add dense mxm test to CMakeLists

---
+++ tests/unit/dense_mxm.cpp
@@ -0,0 +1,96 @@
+
+/*
+ *   Copyright 2021 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include <graphblas.hpp>
+
+using namespace grb;
+
+void grb_program( const size_t & n, grb::RC & rc ) {
+	grb::Semiring< grb::operators::add< double >, grb::operators::mul< double >, grb::identities::zero, grb::identities::one > ring;
+
+	std::cout << "\tTesting dense mxm\n";
+	// initialize test
+	grb::Matrix< double > A( n, n );
+	grb::Matrix< double > B( n, n );
+	grb::Matrix< double > C( n, n );
+
+	std::cout << "_GRB_BACKEND = " << _GRB_BACKEND << "\n";
+	
+	#ifdef _GRB_WITH_REFERENCE
+		std::cout << "_GRB_WITH_REFERENCE defined\n";
+	#endif
+
+	#ifdef _GRB_WITH_DENSE
+		std::cout << "_GRB_WITH_DENSE defined\n";
+	#endif
+
+	rc = grb::SUCCESS;
+
+}
+
+int main( int argc, char ** argv ) {
+	// defaults
+	bool printUsage = false;
+	size_t in = 100;
+
+	// error checking
+	if( argc > 2 ) {
+		printUsage = true;
+	}
+	if( argc == 2 ) {
+		size_t read;
+		std::istringstream ss( argv[ 1 ] );
+		if( ! ( ss >> read ) ) {
+			std::cerr << "Error parsing first argument\n";
+			printUsage = true;
+		} else if( ! ss.eof() ) {
+			std::cerr << "Error parsing first argument\n";
+			printUsage = true;
+		} else if( read % 2 != 0 ) {
+			std::cerr << "Given value for n is odd\n";
+			printUsage = true;
+		} else {
+			// all OK
+			in = read;
+		}
+	}
+	if( printUsage ) {
+		std::cerr << "Usage: " << argv[ 0 ] << " [n]\n";
+		std::cerr << "  -n (optional, default is 100): an even integer, the "
+					 "test size.\n";
+		return 1;
+	}
+
+	std::cout << "This is functional test " << argv[ 0 ] << "\n";
+	grb::Launcher< AUTOMATIC > launcher;
+	grb::RC out;
+	if( launcher.exec( &grb_program, in, out, true ) != SUCCESS ) {
+		std::cerr << "Launching test FAILED\n";
+		return 255;
+	}
+	if( out != SUCCESS ) {
+		std::cerr << "Test FAILED (" << grb::toString( out ) << ")" << std::endl;
+	} else {
+		std::cout << "Test OK" << std::endl;
+	}
+	return 0;
+}
+
