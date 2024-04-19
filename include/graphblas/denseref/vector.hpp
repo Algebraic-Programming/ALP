0a69fcb934bf3ed4340d843826db62f3d7a0580d
Daniele G. Spampinato <daniele.giuseppe.spampinato@huawei.com>
2024-04-19 14:04:56 +0200

    # This is a combination of 7 commits. # This is the 1st commit message:
    Issue #303: framework for reference_dense added
    
    This commit comes from a split of the original commit whose details are as follows:
    SHA1 ID: a7c1db0eb45326184dadec39a6a7731eb16a39fd
    Author: Albert-Jan Yzelman <yzelman@linux.fritz.box>  2022-01-14 15:52:26
    Committer: Albert-Jan Yzelman <yzelman@linux.fritz.box>  2022-01-30 13:48:35
    
    The reason for splitting is to separate the changes irrelevant to the branch 303 into a separate branch.
    The split produced 3 separate commits.
    This commit is number 1.
    This commit in particular is relevant to 303 and will remain in it.
    
    # This is the commit message #2:
    
    Issue #303: unify script argument similar to --no-reference
    
    # This is the commit message #3:
    
    Issue #303: complete implementation of Vector< reference_dense >, modulo output iteration.
    
    # This is the commit message #4:
    
    Issue #303: modified vector implementation according to option 3 in handling dense container construction.
    
    # This is the commit message #5:
    
    Issue #303: vectors now have an iterator for the user to extract data from.
    
    # This is the commit message #6:
    
    Issue #303, fixed silly oops: the difference between two iterators should be an integer
    
    # This is the commit message #7:
    
    Issue #303: containers are now uninitialized on construction. Iteration over uninitialized containers returns nothing.

--- include/graphblas/denseref/vector.hpp
+++ include/graphblas/denseref/vector.hpp
@@ -23,12 +23,175 @@
 #ifndef _H_GRB_DENSEREF_VECTOR
 #define _H_GRB_DENSEREF_VECTOR
 
+#include <graphblas/rc.hpp>
+#include <graphblas/utils/alloc.hpp>
+#include <graphblas/utils/autodeleter.hpp>
+
+#include <graphblas/denseref/vectoriterator.hpp>
+
+#include <stdexcept>
+
+#include <assert.h>
+
+
 namespace grb {
+
+	namespace internal {
+
+		template< typename T >
+		T * getRaw( Vector< T, reference_dense, void > & ) noexcept;
+
+		template< typename T >
+		const T * getRaw( Vector< T, reference_dense, void > & ) noexcept;
+
+		template< typename T >
+		size_t getLength( const Vector< T, reference_dense, void > & ) noexcept;
+
+	} // end namespace ``grb::internal''
 
 	/** \internal TODO */
 	template< typename T >
 	class Vector< T, reference_dense, void > {
+
+		friend T * internal::getRaw< T >( Vector< T, reference_dense, void > & ) noexcept;
+		friend const T * internal::getRaw< T >( const Vector< T, reference_dense, void > & ) noexcept;
+		friend size_t internal::getLength< T >( const Vector< T, reference_dense, void > & ) noexcept;
+
+		private:
+
+			/** The length of the vector. */
+			size_t n;
+
+			/** The vector data. */
+			T *__restrict__ data;
+
+			/** Deleter corresponding to #data. */
+			utils::AutoDeleter< T > data_deleter;
+
+			/** Whether the container presently is uninitialized. */
+			bool initialized;
+
+
+		public:
+
+			/** Exposes the element type. */
+			typedef T value_type;
+
+			/** The return type of #operator[](). */
+			typedef T& lambda_reference;
+
+			/** The iterator type. */
+			typedef typename internal::ConstDenserefVectorIterator< T, reference_dense > const_iterator;
+
+			/**
+			 * @param[in] length The requested vector length.
+			 *
+			 * \internal Allocates a single array of size \a length.
+			 */
+			Vector( const size_t length ) : n( length ), initialized( false ) {
+				const RC rc = grb::utils::alloc(
+					"grb::Vector< T, reference_dense > (constructor)", "",
+					data, length, true, data_deleter
+				);
+				if( rc == OUTOFMEM ) {
+					throw std::runtime_error( "Out-of-memory during Vector< T, reference_dense > construction" );
+				} else if( rc != SUCCESS ) {
+					throw std::runtime_error( "Unhandled runtime error during Vector< T, reference_dense > construction " );
+				}
+			}
+
+			/** \internal Simply calls Vector( 0 ). */
+			Vector() : Vector( 0 ) {}
+
+			/** \internal Makes a deep copy of \a other. */
+			Vector( const Vector< T, reference_dense, void > &other ) : Vector( other.n ) {
+				initialized = false;
+				const RC rc = set( *this, other ); // note: initialized will be set as part of this call
+				if( rc != SUCCESS ) {
+					throw std::runtime_error( "grb::Vector< T, reference_dense > (copy constructor): error during call to grb::set (" + toString( rc ) + ")" );
+				}
+			}
+
+			/** \internal No implementation notes. */
+			Vector( Vector< T, reference_dense, void > &&other ) {
+				n = other.n; other.n = 0;
+				data = other.data; other.data = 0;
+				data_deleter = std::move( other.data_deleter );
+				initialized = other.initialized; other.initialized = false;
+			}
+
+			/** \internal No implementation notes. */
+			~Vector() {
+				n = 0;
+				initialized = false;
+				// free of data will be handled by #data_deleter
+			}
+
+			/** \internal No implementation notes. */
+			lambda_reference operator[]( const size_t i ) noexcept {
+				assert( i < n );
+				assert( initialized );
+				return data[ i ];
+			}
+
+			/** \internal No implementation notes. */
+			const lambda_reference operator[]( const size_t i ) const noexcept {
+				assert( i < n );
+				assert( initialized );
+				return data[ i ];
+			}
+
+			/** \internal Relies on #internal::ConstDenserefVectorIterator. */
+			const_iterator cbegin() const noexcept {
+				return initialized ?
+					const_iterator( data, n, false ) :
+					const_iterator( nullptr, 0, false );
+			}
+
+			/** \internal Relies on #internal::ConstDenserefVectorIterator. */
+			const_iterator begin() const noexcept {
+				return cbegin();
+			}
+
+			/** \internal Relies on #internal::ConstDenserefVectorIterator. */
+			const_iterator cend() const noexcept {
+				return initialized ?
+					const_iterator( data, n, true ) :
+					const_iterator( nullptr, 0, true );
+			}
+
+			/** \internal Relies on #internal::ConstDenserefVectorIterator. */
+			const_iterator end() const noexcept {
+				return cend();
+			}
+
 	};
+
+	/** Identifies any reference_dense vector as an ALP vector. */
+	template< typename T >
+	struct is_container< Vector< T, reference_dense, void > > {
+		/** A reference_vector is an ALP object. */
+		static const constexpr bool value = true;
+	};
+
+	namespace internal {
+
+		template< typename T >
+		T * getRaw( Vector< T, reference_dense, void > &v ) noexcept {
+			return v.data;
+		}
+
+		template< typename T >
+		const T * getRaw( Vector< T, reference_dense, void > &v ) noexcept {
+			return v.data;
+		}
+
+		template< typename T >
+		size_t getLength( const Vector< T, reference_dense, void > &v ) noexcept {
+			return v.n;
+		}
+
+	} // end namespace ``grb::internal''
 
 } // end namespace ``grb''
 
