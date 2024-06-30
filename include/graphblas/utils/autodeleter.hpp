56f3b267c6bf49eeda56feaad5650ab524663db7
Albert-Jan Yzelman <albertjan.yzelman@huawei.com>
2024-06-30 16:56:05 +0200

    Code review auto-deleter

--- include/graphblas/utils/autodeleter.hpp
+++ include/graphblas/utils/autodeleter.hpp
@@ -35,40 +35,83 @@
 
 #include "graphblas/config.hpp"
 
+
 namespace grb {
 
 	namespace utils {
 
 		namespace internal {
 
+			/**
+			 * A collection of pre-defined deleter functions and functors.
+			 *
+			 * @tparam implementation Which backend the deleter functions apply to.
+			 *
+			 * This is the default implementation for all backends.
+			 *
+			 * \note Typically this collection only needs be overridden for targets that
+			 *       require specialised memory management routines.
+			 */
 			template< enum Backend implementation >
 			class DeleterFunctions {
-			private:
-				/** Prevent instantiation. */
-				DeleterFunctions() {}
 
-			public:
-				/** \todo documentation */
-				template< typename T >
-				static void safe_free( T * const pointer ) {
-					if( pointer != NULL ) {
-						free( pointer );
-					}
-				}
-
-#ifndef _GRB_NO_LIBNUMA
-				/** \todo documentation */
-				template< typename T >
-				class safe_numa_free {
 				private:
-					size_t size;
+
+					/** Prevent instantiation. */
+					DeleterFunctions() = delete;
+
 
 				public:
-					safe_numa_free( const size_t size_in ) : size( size_in ) {}
-					void operator()( T * const pointer ) {
-						numa_free( pointer, size );
+
+					/**
+					 * Frees a generic pointer allocated using malloc or posix_memalign.
+					 *
+					 * @tparam T The type of the pointer.
+					 *
+					 * @param[in] pointer Pointer to the memory address to be freed.
+					 */
+					template< typename T >
+					static void safe_free( T * const pointer ) {
+						if( pointer != nullptr ) {
+							free( pointer );
+						}
 					}
-				};
+
+#ifndef _GRB_NO_LIBNUMA
+					/**
+					 * Functor that frees a pointer to a memory region allocated via libnuma.
+					 *
+					 * @tparam T The type of the pointer.
+					 */
+					template< typename T >
+					class safe_numa_free {
+
+						private:
+
+							/** The size of the memory region. */
+							size_t size;
+
+							safe_numa_free() = delete;
+
+						public:
+
+							/**
+							 * Functor constructor.
+							 *
+							 * @param[in] size_in The size of the memory region.
+							 */
+							safe_numa_free( const size_t size_in ) : size( size_in ) {}
+
+							/**
+							 * Frees the given memory region allocated via libnuma.
+							 *
+							 * @param[in] pointer Pointer to the memory region.
+							 */
+							void operator()( T * const pointer ) {
+								numa_free( pointer, size );
+							}
+
+					};
 #endif
 			};
 
@@ -79,15 +122,40 @@ namespace grb {
 } // namespace grb
 
 // now define user API:
+
 namespace grb {
 
 	namespace utils {
 
 		/**
-		 * This function is compatible with \a posix_memalign and the standard
-		 * practice to allow \a NULL pointers for empty arrays.
+		 * ALP uses memory regions of two types:
+		 *  -# memory or buffers tied to a container; or
+		 *  -# memory or buffers tied to global and/or thread-local contexts.
 		 *
-		 * \todo expand documentation
+		 * These memory regions may be used by multiple threads simultaneously, and
+		 * may be used by any primitive (that takes the container that owns the data
+		 * region as argument).
+		 *
+		 * ALP memory is always \em initially tied to an ALP context, and an ALP
+		 * context may consist of multiple user processes. However, containers may be
+		 * \em pinned to memory, in order to escape the termination of the ALP context
+		 * in which it was created. For this reason there is not always a single owner
+		 * of a memory region. Memory once pinned may not be accessed or used in
+		 * exactly the same manner as prior to pinning-- consider, for example,
+		 * pinning a container in sequential mode in a distributed-memory parallel
+		 * context.
+		 *
+		 * For these reasons, ALP requires something alike a shared pointer in that it
+		 * requires keeping track of all contexts in which a memory region is used:
+		 * within ALP contexts, and as part of pinning. It is unlike a shared pointer
+		 * in that the stored memory region may be interpreted differently. This class
+		 * therefore decouples the interpretation cq. storage of the pointer from the
+		 * management of the raw memory area: it only provides reference counting, and
+		 * once the count reaches zero, executes the given destructor.
+		 *
+		 * \note This class is compatible with \a posix_memalign.
+		 *
+		 * \note This class handles <tt>nullptr</tt> OK.
 		 *
 		 * \warning This function is \em not thread safe!
 		 *
@@ -102,81 +170,100 @@ namespace grb {
 		template< typename T, enum Backend implementation = config::default_backend >
 		class AutoDeleter {
 
-		private:
-			/** Where the implementation of the free function(s) reside. */
-			typedef typename internal::DeleterFunctions< implementation > functions;
+			private:
 
-			/** Functionality is provided by shared pointer. */
-			std::shared_ptr< T > _shPtr;
+				/** Where the implementation of the free function(s) reside. */
+				typedef typename internal::DeleterFunctions< implementation > functions;
 
-		public:
-			/**
-			 * Constructs a new AutoDeleter from a pointer. When this instance and all
-			 * instances copied from this one are destroyed, the pointer will be freed
-			 * if and only if it is not equal to \a NULL.
-			 *
-			 * The pointer is assumed to be allocated using \a posix_memalign, which is
-			 * compitable with the C-standard \a free. Thus pointers that cannot be
-			 * free'd in this manner should never be passed to this AutoDeleter
-			 * constructor.
-			 *
-			 * @throws std::bad_alloc If the system cannot allocate enough memory.
-			 */
-			AutoDeleter( T * const pointer = NULL, const size_t size = 0 ) {
+				/** Functionality is provided by shared pointer. */
+				std::shared_ptr< T > _shPtr;
+
+
+			public:
+
+				/**
+				 * Constructs a new AutoDeleter from a pointer. When this instance and all
+				 * instances copied from this one are destroyed, the pointer will be freed
+				 * if and only if it is not equal to <tt>nullptr</tt>.
+				 *
+				 * @param[in] pointer The pointer to free if the reference counter reaches
+				 *                    zero.
+				 * @param[in] size If \a size is zero, uses <tt>free</tt> as the destructor.
+				 *                 Otherwise, it uses the free provided by libnuma.
+				 *
+				 * \note If ALP is configured without libnuma (<tt>_GRB_NO_LIBNUMA</tt>),
+				 *       then \a size is ignored.
+				 *
+				 * \note If \a pointer is <tt>nullptr</tt> then the destructor shall never
+				 *       be a no-op.
+				 *
+				 * @throws std::bad_alloc If the system cannot allocate enough memory.
+				 */
+				AutoDeleter( T * const pointer = nullptr, const size_t size = 0 ) {
 #ifdef _GRB_NO_LIBNUMA
-				(void)size;
-				const auto free_p = &( functions::template safe_free< T > );
-				_shPtr = std::shared_ptr< T >( pointer, free_p );
-#else
-				if( size > 0 ) {
-					typedef typename functions::template safe_numa_free< T > FreeFunctor;
-					const FreeFunctor free_f( size );
-					_shPtr = std::shared_ptr< T >( pointer, free_f );
-				} else {
+					(void) size;
 					const auto free_p = &( functions::template safe_free< T > );
 					_shPtr = std::shared_ptr< T >( pointer, free_p );
-				}
+#else
+					if( size > 0 ) {
+						typedef typename functions::template safe_numa_free< T > FreeFunctor;
+						const FreeFunctor free_f( size );
+						_shPtr = std::shared_ptr< T >( pointer, free_f );
+					} else {
+						const auto free_p = &( functions::template safe_free< T > );
+						_shPtr = std::shared_ptr< T >( pointer, free_p );
+					}
 #endif
-			};
+				}
 
-			/**
-			 * Copies an \a other AutoDeleter. The underlying pointer will only be freed
-			 * if at least this new AutoDeleter and the \a other AutoDeleter are
-			 * destroyed. (The preceding says `at least' because other copies may have
-			 * been made previously.)
-			 *
-			 * @throws std::bad_alloc If the system cannot allocate enough memory.
-			 */
-			AutoDeleter( const AutoDeleter< T > & other ) : _shPtr( other._shPtr ) {}
+				/**
+				 * Copies an \a other AutoDeleter. The underlying pointer will only be freed
+				 * if at least this new AutoDeleter and the \a other AutoDeleter are
+				 * destroyed. (The preceding says `at least' because other copies may have
+				 * been made previously.)
+				 *
+				 * @throws std::bad_alloc If the system cannot allocate enough memory.
+				 */
+				AutoDeleter( const AutoDeleter< T > &other ) : _shPtr( other._shPtr ) {}
 
-			/**
-			 * Creates an AutoDeleter from a temporary instance by stealing its
-			 * resources.
-			 */
-			AutoDeleter( AutoDeleter< T > && other ) noexcept {
-				_shPtr = std::move( other._shPtr );
-			}
+				/**
+				 * Creates an AutoDeleter from a temporary instance.
+				 */
+				AutoDeleter( AutoDeleter< T > &&other ) noexcept {
+					_shPtr = std::move( other._shPtr );
+				}
 
-			/** Signals auto-deletion no longer is necessary. */
-			void clear() noexcept {
-				_shPtr.reset();
-			}
+				/**
+				 * Forgets the stored pointer (and decreases its reference counter by one).
+				 */
+				void clear() noexcept {
+					_shPtr.reset();
+				}
 
-			/**
-			 * Relies on std::move. Equals-operator only works on temporary RHS.
-			 */
-			AutoDeleter< T > & operator=( AutoDeleter< T > && other ) {
-				_shPtr = std::move( other._shPtr );
-				return *this;
-			}
+				/**
+				 * Relies on std::move. Equals-operator only works on temporary RHS.
+				 */
+				AutoDeleter< T > & operator=( AutoDeleter< T > &&other ) {
+					clear();
+					_shPtr = std::move( other._shPtr );
+					return *this;
+				}
 
-			/**
-			 * Relies on copying the underlying shared pointer.
-			 */
-			AutoDeleter< T > & operator=( const AutoDeleter< T > & other ) {
-				_shPtr = other._shPtr;
-				return *this;
-			}
+				/**
+				 * Relies on copying the underlying shared pointer.
+				 */
+				AutoDeleter< T > & operator=( const AutoDeleter< T > &other ) {
+					_shPtr = other._shPtr;
+					return *this;
+				}
+
+				/**
+				 * Swaps two auto-deleters.
+				 */
+				void swap( AutoDeleter< T > &other ) {
+					_shPtr = std::swap( other._shPtr );
+				}
+
 		};
 
 	} // namespace utils
@@ -185,7 +272,8 @@ namespace grb {
 
 // include specialised DeleterFunctions
 #ifdef _GRB_WITH_BANSHEE
-#include "graphblas/banshee/deleters.hpp"
+ #include "graphblas/banshee/deleters.hpp"
 #endif
 
 #endif
+
