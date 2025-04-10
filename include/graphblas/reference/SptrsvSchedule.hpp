f9bf41d7d5ee880c43fd6ef76ff2d00d893bb18e
Albert-Jan N. Yzelman <albertjan.yzelman@huawei.com>
2025-04-10 18:46:38 +0200

    Silly bug causing difficult-to-track memory errors, apologies

--- include/graphblas/reference/SptrsvSchedule.hpp
+++ include/graphblas/reference/SptrsvSchedule.hpp
@@ -194,14 +194,14 @@ namespace grb {
 							"grb::internal::SptrsvSchedule (default constructor)",
 							"default thread-local data allocation, variant I",
 							data[ s ], 2 * nRanges_in * sizeof(NIT), false, _deleters[ s ],
-							endPositions[ s ], supersteps * sizeof( NIT ), false, _deleters[ 2 * s ]
+							endPositions[ s ], supersteps * sizeof( NIT ), false, _deleters[ s + T ]
 						);
 					} else {
 						data[ s ] = nullptr;
 						rc = utils::alloc(
 							"grb::internal::SptrsvSchedule (default constructor)",
 							"default thread-local data allocation, variant II",
-							endPositions[ s ], supersteps * sizeof( NIT ), false, _deleters[ 2 * s ]
+							endPositions[ s ], supersteps * sizeof( NIT ), false, _deleters[ s + T ]
 						);
 					}
 					if( rc != grb::SUCCESS ) {
