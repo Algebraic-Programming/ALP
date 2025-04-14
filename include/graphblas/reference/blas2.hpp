ad392992e1b381163975e05fd363213e38abe9f9
tonibohnlein <113011201+tonibohnlein@users.noreply.github.com>
2025-04-14 11:05:08 +0200

    fixed bug in blas2.hpp

--- include/graphblas/reference/blas2.hpp
+++ include/graphblas/reference/blas2.hpp
@@ -2298,7 +2298,8 @@ namespace grb {
 						(void) grb::foldl( v_raw[ i ], divBy, division );
 					}
 				} else {
-					for( size_t i = offset; i < n; ++i ) {
+					for( size_t j = 0; j < n; ++j ) {
+						const size_t i = j + offset;
 						IOType divBy = semiring.template getZero< IOType >();
 						assert( crs.col_start[ i ] <= crs.col_start[ i + 1 ] );
 						sptrsv_kernel( crs, i, v_raw, divBy, semiring, subtraction );
