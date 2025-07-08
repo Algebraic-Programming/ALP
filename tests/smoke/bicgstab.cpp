2b32ce7233808487e8467b2c441e8703e1a2c583
Albert-Jan N. Yzelman <albertjan.yzelman@huawei.com>
2025-07-08 11:20:10 +0200

    Check error code returned by wait, make wait optional in hot loop, resolve pre-existing bug where some error codes were not checked

--- tests/smoke/bicgstab.cpp
+++ tests/smoke/bicgstab.cpp
@@ -178,6 +178,7 @@ void grbProgram( const struct input &data_in, struct output &out ) {
 			>( data.cend() ),
 			PARALLEL
 		);*/
+		rc = rc ? rc : wait();
 		if( rc != SUCCESS ) {
 			std::cerr << "Failure: call to buildMatrixUnique did not succeed "
 				<< "(" << toString( rc ) << ")." << std::endl;
@@ -197,7 +198,6 @@ void grbProgram( const struct input &data_in, struct output &out ) {
 
 	// I/O done
 	out.times.io = timer.time();
-	grb::wait();
 	timer.reset();
 
 	// set up test of default BiCGstab
@@ -210,18 +210,20 @@ void grbProgram( const struct input &data_in, struct output &out ) {
 	Vector< double > x( n ), b( n ), r( n ),
 		buf1( n ), buf2( n ), buf3( n ), buf4( n ), buf5( n );
 
-	set( x, static_cast< double >( 1 ) / static_cast< double >( n ) );
-	set( b, static_cast< double >( 1 ) );
+	RC rc = set( x, static_cast< double >( 1 ) / static_cast< double >( n ) );
+	assert( rc == SUCCESS );
+	rc = rc ? rc : set( b, static_cast< double >( 1 ) );
+	assert( rc == SUCCESS );
+	rc = rc ? rc : wait();
+	assert( rc == SUCCESS );
 
-	grb::wait();
+	// done with preparations
 	out.times.preamble = timer.time();
 
 	// by default, copy input requested repetitions to output repititions performed
 	out.rep = data_in.rep;
 	// time a single call
-	RC rc = SUCCESS;
 	if( out.rep == 0 ) {
-		grb::wait();
 		timer.reset();
 		rc = bicgstab(
 			x, L, b,
@@ -230,7 +232,7 @@ void grbProgram( const struct input &data_in, struct output &out ) {
 			r, buf1, buf2, buf3, buf4, buf5,
 			ring, minus, divide
 		);
-		grb::wait();
+		rc = rc ? rc : wait();
 		double single_time = timer.time();
 		if( !(rc == SUCCESS || rc == FAILED) ) {
 			std::cerr << "Failure: call to BiCGstab not succeed ("
@@ -264,22 +266,22 @@ void grbProgram( const struct input &data_in, struct output &out ) {
 	} else {
 		// do benchmark
 		double time_taken;
-		grb::wait();
 		timer.reset();
 		for( size_t i = 0; i < out.rep && rc == SUCCESS; ++i ) {
-
 			rc = set( x, static_cast< double >( 1 ) / static_cast< double >( n ) );
-
-			if( rc == SUCCESS ) {
-				rc = bicgstab(
+			assert( rc == SUCCESS );
+			rc = rc ? rc : bicgstab(
 					x, L, b,
 					data_in.solver_iterations, tol,
 					out.iterations, out.residual,
 					r, buf1, buf2, buf3, buf4, buf5
 				);
+			assert( rc == SUCCESS );
+			if( Properties<>::isNonblockingExecution ) {
+				rc = rc ? rc : wait();
+				assert( rc == SUCCESS );
 			}
 		}
-		grb::wait();
 		time_taken = timer.time();
 		out.times.useful = time_taken / static_cast< double >( out.rep );
 		// print timing at root process
@@ -296,7 +298,6 @@ void grbProgram( const struct input &data_in, struct output &out ) {
 	}
 
 	// start postamble
-	grb::wait();
 	timer.reset();
 
 	// set error code
