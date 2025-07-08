c50f70c51bca3b2783a2971738ff5f31d5360dd5
Albert-Jan N. Yzelman <albertjan.yzelman@huawei.com>
2025-07-08 16:08:34 +0200

    Error-check wait, remove superfluous wait, make remaining wait conditional

--- tests/smoke/simple_pagerank_from_mpi.cpp
+++ tests/smoke/simple_pagerank_from_mpi.cpp
@@ -83,11 +83,12 @@ void grbProgram( const input_matrix &A, struct output_vector &out ) {
 
 	double time_taken;
 	grb::utils::Timer timer;
-	grb::wait();
 	timer.reset();
 	rc = simple_pagerank< descriptors::no_operation >( pr, L, buf1, buf2, buf3,
 		buf4 );
-	grb::wait();
+	if( Properties<>::isNonblockingExecution ) {
+		rc = rc ? rc : wait();
+	}
 	time_taken = timer.time();
 
 	// print timing at root process
