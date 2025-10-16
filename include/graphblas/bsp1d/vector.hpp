7e834b992e62b3eccf6453f85d941f7ee307f6c8
Giovanni Gaio <48856010+GiovaGa@users.noreply.github.com>
2025-10-16 17:21:49 +0200

    168 Added copy-assignment vector ID unit tests (#382)
    Prior to this MR, the implementation guarantees retention of IDs when subject to copy-assignment. However, under move-assignment, the moved-to container relinquishes its resources, taking over the resources of the moved-from container instead. This implies that the moved-to container takes over the ID of the moved-from container (while the ID, and general state, of the moved-from container becomes undefined). This issue was previously detected and fixed for ALP/GraphBLAS matrices in #279 / #286, which also introduces a unit test for preventing regressions.
    
    This MR introduces a unit test for move-assignment of vectors, which confirmed that ALP prior to this MR had invalid behaviour re IDs when move-assigning vectors, as issue #168 indeed suspected. This MR fixes those issues, and includes two minor additional code style fixes.

--- include/graphblas/bsp1d/vector.hpp
+++ include/graphblas/bsp1d/vector.hpp
@@ -70,7 +70,7 @@ namespace grb {
 		}
 
 		template< typename DataType, typename Coords >
-		void setDense( Vector< DataType, BSP1D, Coords > & x );
+		void setDense( Vector< DataType, BSP1D, Coords > &x );
 
 		template<
 			Descriptor descr,
@@ -2554,6 +2554,12 @@ namespace grb {
 		 * @see grb::Vector for the user-level specfication.
 		 */
 		Vector< D, BSP1D, C > & operator=( Vector< D, BSP1D, C > &&x ) noexcept {
+			// get thread-local store
+			auto &data = internal::grb_BSP1D.load();
+			// free container ID
+			if( _n > 0 ) {
+				data.mapper.remove( _id );
+			}
 			// move all fields from x to our instance
 			_id = x._id;
 			_raw = x._raw;
